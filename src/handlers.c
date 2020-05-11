02bffb009f0c5815209e6a477777d95d72574df2
Pete Johanson <peter@peterjohanson.com>
2020-05-11 14:42:59 -0400

    Pass pointer to event to user handler.
    * Allow user functions to modify/update the event if they
      need to.

--- src/handlers.c
+++ src/handlers.c
@@ -3,17 +3,18 @@
 
 #include "usb_hid.h"
 
-__attribute__((weak)) bool zmk_handle_key_user(struct zmk_key_event key_event)
+__attribute__((weak)) bool zmk_handle_key_user(struct zmk_key_event *key_event)
 {
 	return true;
 };
 
 void zmk_handle_key(struct zmk_key_event key_event)
 {
-	if (!zmk_handle_key_user(key_event))
+	if (!zmk_handle_key_user(&key_event))
 	{
 		return;
 	}
+
 	if (key_event.pressed)
 	{
 		zmk_usb_hid_press_key(key_event.key);
