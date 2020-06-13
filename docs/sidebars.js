30985517b82b1641b15822b4f6c5531638b2f823
innovaker <66737976+innovaker@users.noreply.github.com>
2020-06-13 18:47:20 +0100

    docs: add faq to sidebars [#11]

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ["intro", "hardware"],
+    'Getting Started': ["intro", "hardware", "faq"],
     Development: ["dev-setup", "dev-clean-room", "dev-posix-board"],
   },
 };
