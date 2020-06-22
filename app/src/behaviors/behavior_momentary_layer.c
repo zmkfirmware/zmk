93635077e600e269aa905a0558c07acf0efebffb
Pete Johanson <peter@peterjohanson.com>
2020-06-21 21:56:49 -0400

    Implement momentary layer + transparent behaviors
    * Implement "momentary layer" behavior with `&mo`
      reference.
    * Implement basic "transparent" behavior with `&trans`
      reference.

---
+++ app/src/behaviors/behavior_momentary_layer.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 Peter Johanson <peter@peterjohanson.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_momentary_layer
+
+#include <device.h>
+#include <drivers/behavior.h>
+#include <logging/log.h>
+
+#include <zmk/events.h>
+#include <zmk/keymap.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+struct behavior_mo_config { };
+struct behavior_mo_data { };
+
+static int behavior_mo_init(struct device *dev)
+{
+	return 0;
+};
+
+
+static int on_keymap_binding_pressed(struct device *dev, u32_t position, u32_t layer, u32_t _)
+{
+  LOG_DBG("position %d layer %d", position, layer);
+
+  return zmk_keymap_layer_activate(layer);
+}
+
+static int on_keymap_binding_released(struct device *dev, u32_t position, u32_t layer, u32_t _)
+{
+  LOG_DBG("position %d layer %d", position, layer);
+
+  return zmk_keymap_layer_deactivate(layer);
+}
+
+static const struct behavior_driver_api behavior_mo_driver_api = {
+  .binding_pressed = on_keymap_binding_pressed,
+  .binding_released = on_keymap_binding_released
+};
+
+
+static const struct behavior_mo_config behavior_mo_config = {};
+
+static struct behavior_mo_data behavior_mo_data;
+
+DEVICE_AND_API_INIT(behavior_mo, DT_INST_LABEL(0), behavior_mo_init,
+                    &behavior_mo_data,
+                    &behavior_mo_config,
+                    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                    &behavior_mo_driver_api);
---
+++ app/src/behaviors/behavior_transparent.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 Peter Johanson <peter@peterjohanson.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_transparent
+
+#include <device.h>
+#include <power/reboot.h>
+#include <drivers/behavior.h>
+#include <logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+struct behavior_transparent_config { };
+struct behavior_transparent_data { };
+
+static int behavior_transparent_init(struct device *dev)
+{
+	return 0;
+};
+
+static int on_keymap_binding_pressed(struct device *dev, u32_t position, u32_t _param1, u32_t _param2)
+{
+  return 1;
+}
+
+static int on_keymap_binding_released(struct device *dev, u32_t position, u32_t _param1, u32_t _param2)
+{
+  return 1;
+}
+
+static const struct behavior_driver_api behavior_transparent_driver_api = {
+  .binding_pressed = on_keymap_binding_pressed,
+  .binding_released = on_keymap_binding_released,
+};
+
+
+static const struct behavior_transparent_config behavior_transparent_config = {};
+
+static struct behavior_transparent_data behavior_transparent_data;
+
+DEVICE_AND_API_INIT(behavior_transparent, DT_INST_LABEL(0), behavior_transparent_init,
+                    &behavior_transparent_data,
+                    &behavior_transparent_config,
+                    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                    &behavior_transparent_driver_api);
