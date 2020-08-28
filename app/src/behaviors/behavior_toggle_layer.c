033bb7bfc4e1772ea19784bd71a57f49b4368e2d
Cody McGinnis <brainwart@gmail.com>
2020-08-28 16:21:31 -0400

    fix(behavior): add logging to toggle layer for tests

--- app/src/behaviors/behavior_toggle_layer.c
+++ app/src/behaviors/behavior_toggle_layer.c
@@ -14,28 +14,35 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-struct behavior_tog_config { };
-struct behavior_tog_data { };
+struct behavior_tog_config
+{
+};
+struct behavior_tog_data
+{
+};
 
 static int behavior_tog_init(struct device *dev)
 {
-	return 0;
+  return 0;
 };
-
 
 static int tog_keymap_binding_pressed(struct device *dev, u32_t position, u32_t layer, u32_t _)
 {
+  LOG_DBG("position %d layer %d", position, layer);
+
   return zmk_keymap_layer_toggle(layer);
 }
 
 static int tog_keymap_binding_released(struct device *dev, u32_t position, u32_t layer, u32_t _)
 {
+  LOG_DBG("position %d layer %d", position, layer);
+
   return 0;
 }
 
 static const struct behavior_driver_api behavior_tog_driver_api = {
-  .binding_pressed = tog_keymap_binding_pressed,
-  .binding_released = tog_keymap_binding_released,
+    .binding_pressed = tog_keymap_binding_pressed,
+    .binding_released = tog_keymap_binding_released,
 };
 
 static const struct behavior_tog_config behavior_tog_config = {};
