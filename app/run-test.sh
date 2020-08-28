f1224422c502cb86c9138c42e28eb645fe62272f
Cody McGinnis <brainwart@gmail.com>
2020-08-28 16:21:31 -0400

    fix(test): stop using pristine and use 4 threads

--- app/run-test.sh
+++ app/run-test.sh
@@ -4,14 +4,14 @@ if [ -z "$1" ]; then
 	echo "Usage: ./run-test.sh <path to testcase>"
 	exit 1
 elif [ "$1" = "all" ]; then
-	find tests -name native_posix.keymap -exec dirname \{\} \; | xargs -l -P 2 ./run-test.sh
+	find tests -name native_posix.keymap -exec dirname \{\} \; | xargs -l -P 4 ./run-test.sh
 	exit $?
 fi
 
 testcase="$1"
 echo "Running $testcase:"
 
-west build --pristine -d build/$testcase -b native_posix -- -DZMK_CONFIG=$testcase > /dev/null
+west build -d build/$testcase -b native_posix -- -DZMK_CONFIG=$testcase > /dev/null
 ./build/$testcase/zephyr/zmk.exe | sed -e "s/.*> //" | sed -n -f $testcase/events.patterns > build/$testcase/keycode_events.log
 
 diff -au $testcase/keycode_events.snapshot build/$testcase/keycode_events.log
