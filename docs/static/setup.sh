595dff68710f7f58e7e257c3d060e80e413dfb87
CrossR <r.cross@lancaster.ac.uk>
2020-09-13 18:07:45 +0100

    Add PowerShell script and tidy up bash script.

--- docs/static/setup.sh
+++ docs/static/setup.sh
@@ -1,14 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+CheckExists() {
+    command_to_run=$1
+    error_message=$2
+        
+    if ! eval "$command_to_run" &> /dev/null; then
+        printf "%s\n" "$error_message"
+        exit
+    fi
+}
+
+CheckExists "command -v git" "git is not installed, and is required for this script!"
+CheckExists "command -v curl" "curl is not installed, and is required for this script!"
+
+CheckExists "git config user.name" "Git username not set!\nRun: git config --global user.name 'My Name'"
+CheckExists "git config user.email" "Git email not set!\nRun: git config --global user.email 'example@myemail.com'"
+
 repo_path="https://github.com/zmkfirmware/zmk-config-split-template.git"
 title="ZMK Config Setup:"
-
-
-# TODO: Check for git being installed
-# TODO: Check for curl being installed
-# TODO: Check for user.name and user.email git configs being set
 
 prompt="Pick an MCU board:"
 options=("nice!nano" "QMK Proton-C" "BlueMicro840 (v1)")
@@ -30,9 +41,6 @@ select opt in "${options[@]}" "Quit"; do
 
     esac
 done
-
-#read -p "Is this board a complete keyboard [yN]: " complete
-#echo "$complete"
 
 echo ""
 echo "Keyboard Shield Selection:"
@@ -67,41 +75,43 @@ if [ "$split" == "n" ]; then
     repo_path="https://github.com/zmkfirmware/zmk-config-template.git"
 fi
 
-read -e -p "Copy in the stock keymap for customization? [Yn]: " copy_keymap
+read -r -e -p "Copy in the stock keymap for customization? [Yn]: " copy_keymap
 
 if [ -z "$copy_keymap" ] || [ "$copy_keymap" == "Y" ] || [ "$copy_keymap" == "y" ]; then copy_keymap="yes"; fi
 
-read -e -p "GitHub Username (leave empty to skip GitHub repo creation): " github_user
+read -r -e -p "GitHub Username (leave empty to skip GitHub repo creation): " github_user
 if [ -n "$github_user" ]; then
-	read -p "GitHub Repo Name [zmk-config]: " repo_name
-	if [ -z "$repo_name" ]; then repo_name="zmk-config"; fi
+    read -r -p "GitHub Repo Name [zmk-config]: " repo_name
+    if [ -z "$repo_name" ]; then repo_name="zmk-config"; fi
 
-	read -p "GitHub Repo [https://github.com/${github_user}/${repo_name}.git]: " github_repo
+    read -r -p "GitHub Repo [https://github.com/${github_user}/${repo_name}.git]: " github_repo
 
-	if [ -z "$github_repo" ]; then github_repo="https://github.com/${github_user}/${repo_name}.git"; fi
+    if [ -z "$github_repo" ]; then github_repo="https://github.com/${github_user}/${repo_name}.git"; fi
 else
-	repo_name="zmk-config"
+    repo_name="zmk-config"
 fi
 
 echo ""
 echo "Preparing a user config for:"
 echo "* MCU Board: ${board}"
 echo "* Shield: ${shield}"
+
 if [ "$copy_keymap" == "yes" ]; then
     echo "* Copy Keymap?: ✓"
 else
     echo "* Copy Keymap?: ❌"
 fi
+
 if [ -n "$github_repo" ]; then
-	echo "* GitHub Repo To Push (please create this in GH first!): ${github_repo}"
+    echo "* GitHub Repo To Push (please create this in GH first!): ${github_repo}"
 fi
 
 echo ""
-read -p "Continue? [Yn]: " do_it
+read -r -p "Continue? [Yn]: " do_it
 
 if [ -n "$do_it" ] && [ "$do_it" != "y" ] && [ "$do_it" != "Y" ]; then
-	echo "Aborting..."
-	exit
+    echo "Aborting..."
+    exit
 fi
 
 git clone --single-branch $repo_path ${repo_name}
@@ -118,10 +128,10 @@ fi
 popd
 
 sed -i'.orig' \
-	-e "s/BOARD_NAME/$board/" \
-	-e "s/SHIELD_NAME/$shield/" \
-	-e "s/KEYBOARD_TITLE/$shield_title/" \
-	.github/workflows/build.yml
+    -e "s/BOARD_NAME/$board/" \
+    -e "s/SHIELD_NAME/$shield/" \
+    -e "s/KEYBOARD_TITLE/$shield_title/" \
+    .github/workflows/build.yml
 
 if [ "$board" == "proton_c" ]; then
     # Proton-C board still fa
@@ -136,11 +146,11 @@ git add .
 git commit -m "Initial User Config."
 
 if [ -n "$github_repo" ]; then
-	git remote add origin "$github_repo"
-	git push --set-upstream origin $(git symbolic-ref --short HEAD)
+    git remote add origin "$github_repo"
+    git push --set-upstream origin "$(git symbolic-ref --short HEAD)"
 
     # TODO: Support determing the actions URL when non-https:// repo URL is used.
     if [ "${github_repo}" != "${github_repo#https://}" ]; then
-        echo "Your firmware should be available from the GitHub Actions shortly: ${github_url%.git}/actions"
+        echo "Your firmware should be available from the GitHub Actions shortly: ${github_repo%.git}/actions"
     fi
 fi
