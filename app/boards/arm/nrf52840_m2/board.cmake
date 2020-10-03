b8cb5f939a3c018af13de7727c15d7765ea1f1c1
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-10-03 18:21:22 +0800

    M60 keymap update and misc changes
    * Updated M60 keymap
    * Make nrfjproj as default runner
    * Remove the polling config

--- app/boards/arm/nrf52840_m2/board.cmake
+++ app/boards/arm/nrf52840_m2/board.cmake
@@ -5,5 +5,5 @@
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
