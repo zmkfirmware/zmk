4d81b10ba7047a4dbd63cfe33ac879ecf437e108
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-10-06 15:52:21 +0800

    Added driver to control the external power output
    This PR adds support to control the external power output from controllers like nice!nano, nRFMicro etc
    
    I have implemented based on my understanding of Pete suggestion on this feature.
    
    Testing done:
    
        Tested by enabling and disabling the ext_power from application and verified
        Verified the application does not crash with boards that does not have ext_power support
        Note:
        I did not test this in nice!nano since I don't have the boards. Will get help from others once the behavior PR is up
    
    Next Steps:
    
        Create a behavior PR to control enable/disable ext_power

--- app/boards/arm/nrfmicro/pinmux.c
+++ app/boards/arm/nrfmicro/pinmux.c
@@ -14,25 +14,14 @@
 static int pinmux_nrfmicro_init(struct device *port) {
     ARG_UNUSED(port);
 
-    struct device *p1 = device_get_binding("GPIO_1");
-
 #if CONFIG_BOARD_NRFMICRO_13
     struct device *p0 = device_get_binding("GPIO_0");
-    // enable EXT_VCC (use 0 for nRFMicro 1.3, use 1 for nRFMicro 1.1)
-    gpio_pin_configure(p1, 9, GPIO_OUTPUT);
-    gpio_pin_set(p1, 9, 0);
-
 #if CONFIG_BOARD_NRFMICRO_CHARGER
     gpio_pin_configure(p0, 5, GPIO_OUTPUT);
     gpio_pin_set(p0, 5, 0);
 #else
     gpio_pin_configure(p0, 5, GPIO_INPUT);
 #endif
-
-#else
-    // enable EXT_VCC (use 0 for nRFMicro 1.3, use 1 for nRFMicro 1.1)
-    gpio_pin_configure(p1, 9, GPIO_OUTPUT);
-    gpio_pin_set(p1, 9, 1);
 #endif
     return 0;
 }
