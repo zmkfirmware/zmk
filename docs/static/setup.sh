43c900f94a9394bc09bdfc646bfd3f42f947d927
Pete Johanson <peter@peterjohanson.com>
2020-10-13 19:46:02 -0400

    feat: Add qaz to setup scripts.

--- docs/static/setup.sh
+++ docs/static/setup.sh
@@ -58,7 +58,7 @@ echo ""
 echo "Keyboard Shield Selection:"
 
 prompt="Pick an keyboard:"
-options=("Kyria" "Lily58" "Corne" "Splitreus62" "Sofle" "Iris" "RoMac" "RoMac+" "makerdiary M60" "Microdox" "TG4X")
+options=("Kyria" "Lily58" "Corne" "Splitreus62" "Sofle" "Iris" "RoMac" "RoMac+" "makerdiary M60" "Microdox" "TG4X" "QAZ")
 
 PS3="$prompt "
 # TODO: Add support for "Other" and linking to docs on adding custom shields in user config repos.
@@ -78,6 +78,7 @@ select opt in "${options[@]}" "Quit"; do
     9 ) shield_title="M60" shield="m60"; split="n"; break;;
     10 ) shield_title="Microdox" shield="microdox"; split="y"; break;;
     11 ) shield_title="TG4X" shield="tg4x"; split="n"; break;;
+    12 ) shield_title="QAZ" shield="qaz"; split="n"; break;;
 
     # Add link to docs on adding your own custom shield in your ZMK config!
     # $(( ${#options[@]}+1 )) ) echo "Other!"; break;;
