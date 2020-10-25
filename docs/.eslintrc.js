ab65bf49db46291dfc429cda4737e894d1897e6f
innovaker <66737976+innovaker@users.noreply.github.com>
2020-10-25 21:23:16 +0000

    docs: Add eslint support
    Official guidelines:
    https://eslint.org/docs/user-guide/getting-started
    
    Steps taken:
    npm install eslint --save-dev
    npx eslint --init
    Create .eslintignore
    Edit .eslintrc.js (tweaks)

---
+++ docs/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
