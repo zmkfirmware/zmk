f4596fc784d11aa2d03ff0b7fa0a921600fc2db2
innovaker <66737976+innovaker@users.noreply.github.com>
2020-10-27 22:35:44 +0000

    refactor(hid): Refactor keypad report to use a configurable integer array
    Replace NKRO bit array with configurable integer (DV) array.

--- app/include/zmk/hid.h
+++ app/include/zmk/hid.h
@@ -9,13 +9,11 @@
 #include <usb/usb_device.h>
 #include <usb/class/usb_hid.h>
 
-#include <dt-bindings/zmk/keys.h>
-
 #include <zmk/keys.h>
 
 #define COLLECTION_REPORT 0x03
 
-#define ZMK_HID_MAX_KEYCODE GUI
+#define ZMK_HID_KEYPAD_NKRO_SIZE 6
 
 static const u8_t zmk_hid_report_desc[] = {
     /* USAGE_PAGE (Generic Desktop) */
@@ -62,36 +60,25 @@ static const u8_t zmk_hid_report_desc[] = {
     /* LOGICAL_MINIMUM (0) */
     HID_GI_LOGICAL_MIN(1),
     0x00,
-    /* LOGICAL_MAXIMUM (1) */
+    /* LOGICAL_MAXIMUM (0xFF) */
     HID_GI_LOGICAL_MAX(1),
-    0x01,
+    0xFF,
     /* USAGE_MINIMUM (Reserved) */
     HID_LI_USAGE_MIN(1),
     0x00,
     /* USAGE_MAXIMUM (Keyboard Application) */
     HID_LI_USAGE_MAX(1),
-    ZMK_HID_MAX_KEYCODE,
-    /* REPORT_SIZE (8) */
+    0xFF,
+    /* REPORT_SIZE (1) */
     HID_GI_REPORT_SIZE,
-    0x01,
-    /* REPORT_COUNT (6) */
+    0x08,
+    /* REPORT_COUNT (ZMK_HID_KEYPAD_NKRO_SIZE) */
     HID_GI_REPORT_COUNT,
-    ZMK_HID_MAX_KEYCODE + 1,
+    ZMK_HID_KEYPAD_NKRO_SIZE,
     /* INPUT (Data,Ary,Abs) */
     HID_MI_INPUT,
-    0x02,
-    /* USAGE_PAGE (Keypad) */
-    HID_GI_USAGE_PAGE,
-    USAGE_GEN_DESKTOP_KEYPAD,
-    /* REPORT_SIZE (8) */
-    HID_GI_REPORT_SIZE,
-    0x02,
-    /* REPORT_COUNT (6) */
-    HID_GI_REPORT_COUNT,
-    0x01,
-    /* INPUT (Cnst,Var,Abs) */
-    HID_MI_INPUT,
-    0x03,
+    0x00,
+
     /* END_COLLECTION */
     HID_MI_COLLECTION_END,
     /* USAGE_PAGE (Consumer) */
@@ -142,7 +129,7 @@ static const u8_t zmk_hid_report_desc[] = {
 
 struct zmk_hid_keypad_report_body {
     zmk_mod_flags modifiers;
-    u8_t keys[13];
+    u8_t keys[ZMK_HID_KEYPAD_NKRO_SIZE];
 } __packed;
 
 struct zmk_hid_keypad_report {
