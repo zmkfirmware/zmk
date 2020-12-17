e89aa1cde82e93bcdac7324751af2cbab4d1d44f
onebigdoor <tyler.walker@gmail.com>
2020-12-17 06:05:34 -0500

    docs(codes): improve iOS keyboard/keypad support data
    Co-authored-by: innovaker <66737976+innovaker@users.noreply.github.com>
    PR: #356
    See: #376

--- docs/src/data/footnotes.js
+++ docs/src/data/footnotes.js
@@ -5,9 +5,13 @@
  */
 
 import example from "@site/docs/codes/_footnotes/example.mdx";
+import iosApplication from "@site/docs/codes/_footnotes/ios-application.mdx";
+import iosPower from "@site/docs/codes/_footnotes/ios-power.mdx";
 import macosPower from "@site/docs/codes/_footnotes/macos-power.mdx";
 
 export default {
   example,
+  iosApplication,
+  iosPower,
   macosPower,
 };
