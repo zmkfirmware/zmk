47f873b038c6468d29d97f855dfc05818f660418
Pete Johanson <peter@peterjohanson.com>
2020-12-20 09:49:42 -0500

    feature(split): behavior locality support.
    * GATT characteristic allowing passng data + behavior
      label to invoke the behavior on the peripheral side.
    * Behaviors have a locality setting to specify where they run.
    * Build reset/power/RGB on peripheral.

--- app/include/zmk/split/bluetooth/service.h
+++ app/include/zmk/split/bluetooth/service.h
@@ -6,5 +6,19 @@
 
 #pragma once
 
+#define ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN 9
+
+struct zmk_split_run_behavior_data {
+    uint8_t position;
+    uint8_t state;
+    uint32_t param1;
+    uint32_t param2;
+} __packed;
+
+struct zmk_split_run_behavior_payload {
+    struct zmk_split_run_behavior_data data;
+    char behavior_dev[ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN];
+} __packed;
+
 int zmk_split_bt_position_pressed(uint8_t position);
 int zmk_split_bt_position_released(uint8_t position);
