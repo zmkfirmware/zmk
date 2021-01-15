40694a09695d43006242ed0966387c3aee4e4c32
innovaker <66737976+innovaker@users.noreply.github.com>
2021-01-15 14:18:11 +0000

    docs: remove redundant slash from "docs/" navigation item
    Aligns with the blog item.
    
    PR: #608

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       items: [
         {
-          to: "docs/",
+          to: "docs",
           activeBasePath: "docs",
           label: "Docs",
           position: "left",
