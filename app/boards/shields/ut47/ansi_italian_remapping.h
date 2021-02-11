c8b0b03d75b81ee40b360224564de045c2c98549
umbacos <umbacos@gmail.com>
2021-02-11 15:35:41 +0100

    ansi italian

---
+++ app/boards/shields/ut47/ansi_italian_remapping.h
@@ -0,0 +1,54 @@
+/* Copyright 2015-2016 Matthias Schmidtt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define IT_EGRV LEFT_BRACKET
+#define IT_AGRV SINGLE_QUOTE
+#define IT_UGRV BACKSLASH
+#define IT_IGRV EQUAL
+#define IT_OGRV SEMICOLON
+#define IT_EURO LA(E)
+#define IT_EXCL LS(N1)
+#define IT_AT   LA(SEMICOLON)
+#define IT_SHRP LA(SINGLE_QUOTE)
+#define IT_DOLL LS(N4)
+#define IT_PERC LS(N5)
+#define IT_POWE LS(EQUAL)
+#define IT_ECOM LS(N6)
+#define IT_STAR LS(RIGHT_BRACKET)
+#define IT_LPAR LS(N8)
+#define IT_RPAR LS(N9)
+#define IT_EQAL LS(N0)
+#define IT_MINU SLASH
+#define IT_PLUS RIGHT_BRACKET
+#define IT_UNDS LS(SLASH)
+#define IT_LSQR LA(LEFT_BRACKET)
+#define IT_RSQR LA(RIGHT_BRACKET)
+#define IT_LBRK LA(LS(LEFT_BRACKET))
+#define IT_RBRK LA(LS(RIGHT_BRACKET))
+#define IT_SCLN LS(COMMA)
+#define IT_COLN LS(DOT)
+#define IT_QUOT MINUS
+#define IT_DQUO LS(N2)
+#define IT_COMM COMMA
+#define IT_FULL DOT
+#define IT_SLSH LS(N7)
+#define IT_QSTN LS(MINUS)
+#define IT_MINO GRAVE
+#define IT_MAJO LS(GRAVE)
+#define IT_BSLS NON_US_HASH //0x64     // Non US backslash
+#define IT_PIPE LS(NON_US_HASH) // Non US pipe |
