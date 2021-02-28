31ec145b5a2cca3b78b73a32f8672220e10eb327
Okke Formsma <okke@formsma.nl>
2021-02-28 21:04:39 +0100

    fix(core): Initialize layer state with base layer activated
    fixes #699

--- app/src/keymap.c
+++ app/src/keymap.c
@@ -19,7 +19,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/sensor_event.h>
 
-static zmk_keymap_layers_state_t _zmk_keymap_layer_state = 0;
+static zmk_keymap_layers_state_t _zmk_keymap_layer_state = 1;
 static uint8_t _zmk_keymap_layer_default = 0;
 
 #define DT_DRV_COMPAT zmk_keymap
