1b9e466c57706c7ae77fd26a162fc84508a0d3f6
JP Bonn <jp-keyword-bitbucket.d58644@jpbonn.com>
2021-03-02 14:17:15 -0700

    removed const from variable

--- app/src/keymap.c
+++ app/src/keymap.c
@@ -19,8 +19,10 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/events/sensor_event.h>
 
-static const uint8_t _zmk_keymap_layer_default = 0;
-static zmk_keymap_layers_state_t _zmk_keymap_layer_state = BIT(_zmk_keymap_layer_default);
+#define ZMK_KEYMAP_INITIAL_DEFAULT_LAYER 0
+
+static uint8_t _zmk_keymap_layer_default = ZMK_KEYMAP_INITIAL_DEFAULT_LAYER;
+static zmk_keymap_layers_state_t _zmk_keymap_layer_state = BIT(ZMK_KEYMAP_INITIAL_DEFAULT_LAYER);
 
 #define DT_DRV_COMPAT zmk_keymap
 
