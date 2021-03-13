f9699eb1bf8248c5b3fd70ea467ad80ebd3ad9d7
KemoNine <mcrosson@kemonine.info>
2021-03-13 19:24:05 +0000

    chore(&sleep): misc code updates from PR #649

--- app/include/zmk/activity.h
+++ app/include/zmk/activity.h
@@ -9,4 +9,4 @@
 enum zmk_activity_state { ZMK_ACTIVITY_ACTIVE, ZMK_ACTIVITY_IDLE, ZMK_ACTIVITY_SLEEP };
 
 enum zmk_activity_state zmk_activity_get_state();
-int activity_set_state(enum zmk_activity_state state);
+int zmk_activity_set_state(enum zmk_activity_state state);
