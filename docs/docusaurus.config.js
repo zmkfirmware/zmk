5e6634d2e522289c5169b22a2a0d8b600a0e008d
Peter Johanson <peter@peterjohanson.com>
2021-03-29 00:59:15 +0000

    feat(docs): Add TS type gen, metadata JSON gen
    * Generate TS types from new hardware metadata schema.
    * Aggregate all hw metadata YAML into one aggregate JSON
      file for consumption by others.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,11 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "zmkfirmware", // Usually your GitHub org/user name.
   projectName: "zmk", // Usually your repo name.
-  plugins: [path.resolve(__dirname, "src/docusaurus-tree-sitter-plugin")],
+  plugins: [
+    path.resolve(__dirname, "src/docusaurus-tree-sitter-plugin"),
+    path.resolve(__dirname, "src/hardware-metadata-collection-plugin"),
+    path.resolve(__dirname, "src/hardware-schema-typescript-plugin")
+  ],
   themeConfig: {
     colorMode: {
       respectPrefersColorScheme: true,
