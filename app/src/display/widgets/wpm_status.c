3cea254908b7ca8af11f4c9e40d42734a678e95f
Peter Johanson <peter@peterjohanson.com>
2021-04-01 21:22:48 -0400

    refactor(display): Saner font selection/defaults.
    * Only select fonts for the default built in status screen
    * Leverage theme default fonts, instead of hardcoding theme
      details in each component.

--- app/src/display/widgets/wpm_status.c
+++ app/src/display/widgets/wpm_status.c
@@ -14,22 +14,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/wpm.h>
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
-static lv_style_t label_style;
-
-static bool style_initialized = false;
-
-void wpm_status_init() {
-    if (style_initialized) {
-        return;
-    }
-
-    style_initialized = true;
-    lv_style_init(&label_style);
-    lv_style_set_text_color(&label_style, LV_STATE_DEFAULT, LV_COLOR_BLACK);
-    lv_style_set_text_font(&label_style, LV_STATE_DEFAULT, &lv_font_montserrat_12);
-    lv_style_set_text_letter_space(&label_style, LV_STATE_DEFAULT, 1);
-    lv_style_set_text_line_space(&label_style, LV_STATE_DEFAULT, 1);
-}
 
 void set_wpm_symbol(lv_obj_t *label, int wpm) {
     char text[4] = {};
@@ -41,9 +25,7 @@ void set_wpm_symbol(lv_obj_t *label, int wpm) {
 }
 
 int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent) {
-    wpm_status_init();
     widget->obj = lv_label_create(parent, NULL);
-    lv_obj_add_style(widget->obj, LV_LABEL_PART_MAIN, &label_style);
     lv_label_set_align(widget->obj, LV_LABEL_ALIGN_RIGHT);
 
     lv_obj_set_size(widget->obj, 40, 15);
