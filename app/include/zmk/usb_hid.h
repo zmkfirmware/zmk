d7d9eed317b3f788370a8728afa449c5e525f2ca
Alexander Krikun <krikun98@gmail.com>
2021-04-27 18:24:11 +0300

    feat(mouse): Initial mouse keys support.
    * Add HID report/descriptor for a new report with mouse buttons,
      and x/y/wheel deltas.
    * New mouse key press behavior for press/release of mouse keys.
    * Add constants for HID main item values (e.g. data/array/absolute)
    * Define and use constants for our HID report IDs.

--- app/include/zmk/usb_hid.h
+++ app/include/zmk/usb_hid.h
@@ -10,4 +10,7 @@
 
 int zmk_usb_hid_send_keyboard_report();
 int zmk_usb_hid_send_consumer_report();
+#if IS_ENABLED(CONFIG_ZMK_MOUSE)
+int zmk_usb_hid_send_mouse_report();
+#endif // IS_ENABLED(CONFIG_ZMK_MOUSE)
 void zmk_usb_hid_set_protocol(uint8_t protocol);
