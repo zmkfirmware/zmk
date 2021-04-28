54ac7658e78dee4eb322774bcb88d64740986705
Alexander Krikun <krikun98@gmail.com>
2021-04-28 15:07:54 +0300

    Fine-tuning report, 16 buttons

--- app/include/zmk/hid.h
+++ app/include/zmk/hid.h
@@ -190,26 +190,24 @@ static const uint8_t zmk_hid_report_desc[] = {
     0x05, 0x01, /* Usage Page (Generic Desktop Ctrls) */
     0x09, 0x02, /* Usage (Mouse) */
     0xA1, 0x01, /* Collection (Application) */
+    0x85, 0x04, /*   Report ID (4) */
     0x09, 0x01, /*   Usage (Pointer) */
     0xA1, 0x00, /*   Collection (Physical) */
     0x05, 0x09, /*     Usage Page (Button) */
     0x19, 0x01, /*     Usage Minimum (0x01) */
-    0x29, 0x03, /*     Usage Maximum (0x03) */
+    0x29, 0x10, /*     Usage Maximum (0x10) */
     0x15, 0x00, /*     Logical Minimum (0) */
     0x25, 0x01, /*     Logical Maximum (1) */
-    0x95, 0x03, /*     Report Count (3) */
+    0x95, 0x10, /*     Report Count (16) */
     0x75, 0x01, /*     Report Size (1) */
     0x81, 0x02, /*     Input (Data,Var,Abs,No Wrap,Linear,...) */
-    0x95, 0x01, /*     Report Count (1) */
-    0x75, 0x05, /*     Report Size (5) */
-    0x81, 0x03, /*     Input (Const,Var,Abs,No Wrap,Linear,...) */
     0x05, 0x01, /*     Usage Page (Generic Desktop Ctrls) */
+    0x16, 0x01, 0x80, /*     Logical Minimum (-32767) */
+    0x26, 0xFF, 0x7F, /*     Logical Maximum (32767) */
+    0x75, 0x10, /*     Report Size (8) */
+    0x95, 0x02, /*     Report Count (2) */
     0x09, 0x30, /*     Usage (X) */
     0x09, 0x31, /*     Usage (Y) */
-    0x15, 0x81, /*     Logical Minimum (129) */
-    0x25, 0x7F, /*     Logical Maximum (127) */
-    0x75, 0x08, /*     Report Size (8) */
-    0x95, 0x02, /*     Report Count (2) */
     0x81, 0x06, /*     Input (Data,Var,Rel,No Wrap,Linear,...) */
     0xC0,       /*   End Collection */
     0xC0,       /* End Collection */
@@ -252,8 +250,8 @@ struct zmk_hid_consumer_report {
 
 struct zmk_hid_mouse_report_body {
     zmk_mouse_button_flags_t buttons;
-    int8_t x;
-    int8_t y;
+    int16_t x;
+    int16_t y;
 } __packed;
 
 struct zmk_hid_mouse_report {
