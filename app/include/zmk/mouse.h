9b0997473472bbd9de32508191e19b98020cbda3
Alexander Krikun <krikun98@gmail.com>
2021-04-28 15:07:54 +0300

    Fine-tuning report, 16 buttons

--- app/include/zmk/mouse.h
+++ app/include/zmk/mouse.h
@@ -9,5 +9,5 @@
 #include <zephyr.h>
 #include <dt-bindings/zmk/mouse.h>
 
-typedef uint8_t zmk_mouse_button_flags_t;
-typedef uint8_t zmk_mouse_button_t;
+typedef uint16_t zmk_mouse_button_flags_t;
+typedef uint16_t zmk_mouse_button_t;
