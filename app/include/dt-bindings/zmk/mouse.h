3fb874f4f679d25da23674af9c8f2b8e94f7f463
Dmitry Tsykunov <>
2021-04-29 04:49:04 +0300

    Mouse-related behaviours

--- app/include/dt-bindings/zmk/mouse.h
+++ app/include/dt-bindings/zmk/mouse.h
@@ -5,5 +5,40 @@
  */
 #pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
+/* Left click */
+#define MB1 (0x01)
+#define LCLK (MB1)
+
+/* Right click */
+#define MB2 (0x02)
+#define RCLK (MB2)
+
+/* Middle click */
+#define MB3 (0x04)
+#define MCLK (MB3)
+
+#define MB4 (0x08)
+
+#define MB5 (0x10)
+
+#define MB6 (0x20)
+
+#define MB7 (0x40)
+
+#define MB8 (0x80)
+
+#define MOVE_UP (0x0000FFFF)
+
+#define MOVE_DOWN (0x00000001)
+
+#define MOVE_LEFT (0xFFFF0000)
+
+#define MOVE_RIGHT (0x00010000)
+
+#define WHEEL_UP (0x0001)
+
+#define WHEEL_DOWN (0x00FF)
+
+#define WHEEL_LEFT (0xFF00)
+
+#define WHEEL_RIGHT (0x0100)
