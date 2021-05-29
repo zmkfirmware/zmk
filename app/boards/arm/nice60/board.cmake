3f838f0aaf90cd5f8dad622fd1d901a9a8eb40d0
Nick Winans <nick@winans.codes>
2021-05-29 10:00:40 -0500

    feat(boards): Add nice!60 board

---
+++ app/boards/arm/nice60/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2021 Nick Winans
+# SPDX-License-Identifier: MIT
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
