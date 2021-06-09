45f8787ba4a2b8994721e8f193742b22779ffa93
Giulio De Donato <liuggio@gmail.com>
2021-06-09 18:33:14 +0200

    added behaviour

--- app/include/dt-bindings/zmk/bt.h
+++ app/include/dt-bindings/zmk/bt.h
@@ -8,13 +8,14 @@
 #define BT_NXT_CMD 1
 #define BT_PRV_CMD 2
 #define BT_SEL_CMD 3
-// #define BT_FULL_RESET_CMD   4
+#define BT_RST_CMD 4
 
 /*
 Note: Some future commands will include additional parameters, so we
 defines these aliases up front.
 */
 
+#define BT_RST BT_RST_CMD 0
 #define BT_CLR BT_CLR_CMD 0
 #define BT_NXT BT_NXT_CMD 0
 #define BT_PRV BT_PRV_CMD 0
