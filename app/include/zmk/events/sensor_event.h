2244bd3d81931753dfd170e804a90a487a205aa1
Peter Johanson <peter@peterjohanson.com>
2021-09-01 03:49:18 +0000

    refactor(sensors): Sensor event channel data, resolution tweaks.
    * Refactor sensor events to include channel data,
      necessary for prop split encoders, and avoiding duplicate calls,
      to fetch channel data twice, etc.
    * More consistent behavior driver API.
    * Allow setting triggers per resolution at the behavior level optionally.

--- app/include/zmk/events/sensor_event.h
+++ app/include/zmk/events/sensor_event.h
@@ -6,12 +6,21 @@
 
 #pragma once
 
-#include <zephyr/kernel.h>
+
+#include <zephyr/drivers/sensor.h>
 #include <zmk/event_manager.h>
-#include <zephyr/device.h>
+#include <zmk/sensors.h>
+#include <device.h>
+
+// TODO: Move to Kconfig when we need more than one channel
+#define ZMK_SENSOR_EVENT_MAX_CHANNELS 1
+
 struct zmk_sensor_event {
-    uint8_t sensor_number;
-    const struct device *sensor;
+    uint8_t sensor_position;
+
+    size_t channel_data_size;
+    struct zmk_sensor_channel_data channel_data[ZMK_SENSOR_EVENT_MAX_CHANNELS];
+
     int64_t timestamp;
 };
 
--- app/src/behaviors/behavior_sensor_rotate_common.h
+++ app/src/behaviors/behavior_sensor_rotate_common.h
@@ -1,5 +1,11 @@
+/*
+ * Copyright (c) 2023 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #include <zmk/behavior.h>
+#include <zmk/sensors.h>
 
 struct behavior_sensor_rotate_config {
     struct zmk_behavior_binding cw_binding;
@@ -8,6 +14,12 @@ struct behavior_sensor_rotate_config {
     bool override_params;
 };
 
+struct behavior_sensor_rotate_data {
+    struct sensor_value remainder[ZMK_KEYMAP_SENSORS_LEN];
+};
+
 int zmk_behavior_sensor_rotate_common_trigger(struct zmk_behavior_binding *binding,
-                                              const struct device *sensor,
-                                              struct zmk_behavior_binding_event event);
+                                              struct zmk_behavior_binding_event event,
+                                              const struct zmk_sensor_config *sensor_config,
+                                              size_t channel_data_size,
+                                              const struct zmk_sensor_channel_data *channel_data);
