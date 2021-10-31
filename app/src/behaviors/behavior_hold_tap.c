99724cbdf8e8775c521c8669c92a3dbab95c3638
jding <jding@roblox.com>
2021-10-31 22:15:37 +0000

    fix missing last lines

--- app/src/behaviors/behavior_hold_tap.c
+++ app/src/behaviors/behavior_hold_tap.c
@@ -664,3 +664,5 @@ static struct behavior_hold_tap_data behavior_hold_tap_data;
                           CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_hold_tap_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(KP_INST)
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
