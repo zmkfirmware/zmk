a05b39b4f877dd0c8af883ca5b870f97d74cae96
Peter Johanson <peter@peterjohanson.com>
2021-11-01 19:25:34 +0000

    feat(display): Add bongo cat widget.

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -8,6 +8,9 @@
 #include <zmk/display/widgets/battery_status.h>
 #include <zmk/display/widgets/layer_status.h>
 #include <zmk/display/widgets/wpm_status.h>
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BONGO_CAT)
+#include <zmk/display/widgets/bongo_cat.h>
+#endif
 #include <zmk/display/status_screen.h>
 
 #include <logging/log.h>
@@ -27,6 +30,10 @@ static struct zmk_widget_layer_status layer_status_widget;
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
 static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BONGO_CAT)
+static struct zmk_widget_bongo_cat bongo_cat_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen() {
@@ -53,6 +60,11 @@ lv_obj_t *zmk_display_status_screen() {
                                      lv_theme_get_font_small());
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
                  0, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_BONGO_CAT)
+    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
