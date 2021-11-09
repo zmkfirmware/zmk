4b965690862e0c0ff4424afe797d68e42e5e089f
Kyle McCreery <mccreery.kyle@gmail.com>
2021-11-09 14:16:13 -0500

    Preparing S40NC for PR

--- app/boards/arm/s40nc/board.cmake
+++ app/boards/arm/s40nc/board.cmake
@@ -1,5 +1,7 @@
+# Copyright (c) 2021 Kyle McCreery
 # SPDX-License-Identifier: MIT
 
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
