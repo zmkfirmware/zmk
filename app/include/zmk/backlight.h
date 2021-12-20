83ce5c77086ecba88c04d6f8997e6bf34fd6ea32
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2021-12-20 11:00:22 +0100

    refactor(backlight): code cleanup

--- app/include/zmk/backlight.h
+++ app/include/zmk/backlight.h
@@ -6,11 +6,11 @@
 
 #pragma once
 
-int zmk_backlight_toggle();
-bool zmk_backlight_get_on();
 int zmk_backlight_on();
 int zmk_backlight_off();
-uint8_t zmk_backlight_calc_brt(int direction);
+int zmk_backlight_toggle();
+bool zmk_backlight_is_on();
+
 int zmk_backlight_set_brt(uint8_t brightness);
-int zmk_backlight_adjust_brt(int direction);
-int zmk_backlight_get_brt();
+uint8_t zmk_backlight_get_brt();
+uint8_t zmk_backlight_calc_brt(int direction);
