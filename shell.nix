8c8c245ba8cf74f0d6efda6272da3441c1abedc8
Dom H <dom@hxy.io>
2022-01-17 05:20:07 +0000

    refactor(setup): More descriptive variable name

--- shell.nix
+++ shell.nix
@@ -13,7 +13,7 @@ let inherit (lib) ;
     url = "https://raw.githubusercontent.com/zephyrproject-rtos/zephyr/v2.5.0/scripts/requirements-base.txt";
     sha256 = "919a78ba9457a8e55451450329158ff7fdcbc4a2ddb0dd76ea804c3b04a3c6c6";
   };
-  machNix = mach-nix.mkPython rec {
+  python-pkgs = mach-nix.mkPython rec {
     requirements = builtins.readFile zephyr-requirements;
   };
 in mkShell {
@@ -24,7 +24,7 @@ in mkShell {
     dtc
     dfu-util
     gcc-arm-embedded
-    machNix
+    python-pkgs
   ];
 
   shellHook = ''
