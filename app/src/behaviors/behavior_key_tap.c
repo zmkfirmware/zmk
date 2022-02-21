cb50620f9fd3d0766054d573c36bd12f4053da59
Marco Sterbik <madbadmax00@gmail.com>
2022-02-21 22:56:19 +0100

    Behaviours: add key-tap behavior
    This is a solution for keys that use the same keycode but different
    modifiers (see #1076 as an example, also { and [ or ] and } might occur
    quite often).
    
    The new key behavior introduced here is a simple tap that immediately
    sends a release event, so that a keypress of the same keycode with a
    different modifier can be detected immediately.

---
+++ app/src/behaviors/behavior_key_tap.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_key_tap
+
+#include <device.h>
+#include <drivers/behavior.h>
+#include <logging/log.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/events/keycode_state_changed.h>
+#include <zmk/behavior.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+static int behavior_key_tap_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    int ret = 0;
+    ret = ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(binding->param1, true, event.timestamp));
+    if(ret != 0) {
+        return 0;
+    }
+    return ZMK_EVENT_RAISE(zmk_keycode_state_changed_from_encoded(binding->param1, false, event.timestamp));
+
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    return 0;
+}
+
+static const struct behavior_driver_api behavior_key_tap_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+
+#define KT_INST(n)                                                                                 \
+    DEVICE_DT_INST_DEFINE(n, behavior_key_tap_init, device_pm_control_nop, NULL, NULL,           \
+                          APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                        \
+                          &behavior_key_tap_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KT_INST)
