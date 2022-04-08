805dd4a53bc9ff7fe1f7eb5c9ea332a9affbc5f9
Peter Johanson <peter@peterjohanson.com>
2022-04-08 15:38:46 +0000

    feat(behaviors): Support parameterized macros.
    * Add two new compatibles for macros that
      take one or two parameters when bound in
      a keymap.
    * Use `&macro_param_1to1`, `&macro_param_1to2`, `&macro_param_2to1`,
      and `&macro_param_2to2` control entries in the bindings for the macro
      to have the next binding entry have it's values substituted.
    
    Co-authored-by: Cem Aksoylar <caksoylar@users.noreply.github.com>

--- app/src/behaviors/behavior_macro.c
+++ app/src/behaviors/behavior_macro.c
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-#define DT_DRV_COMPAT zmk_behavior_macro
-
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
@@ -15,13 +13,13 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-
 enum behavior_macro_mode {
     MACRO_MODE_TAP,
     MACRO_MODE_PRESS,
     MACRO_MODE_RELEASE,
 };
+
+enum param_source { PARAM_SOURCE_BINDING, PARAM_SOURCE_MACRO_1ST, PARAM_SOURCE_MACRO_2ND };
 
 struct behavior_macro_trigger_state {
     uint32_t wait_ms;
@@ -29,6 +27,8 @@ struct behavior_macro_trigger_state {
     enum behavior_macro_mode mode;
     uint16_t start_index;
     uint16_t count;
+    enum param_source param1_source;
+    enum param_source param2_source;
 };
 
 struct behavior_macro_state {
@@ -52,6 +52,11 @@ struct behavior_macro_config {
 #define WAIT_TIME DT_PROP(DT_INST(0, zmk_macro_control_wait_time), label)
 #define WAIT_REL DT_PROP(DT_INST(0, zmk_macro_pause_for_release), label)
 
+#define P1TO1 DT_PROP(DT_INST(0, zmk_macro_param_1to1), label)
+#define P1TO2 DT_PROP(DT_INST(0, zmk_macro_param_1to2), label)
+#define P2TO1 DT_PROP(DT_INST(0, zmk_macro_param_2to1), label)
+#define P2TO2 DT_PROP(DT_INST(0, zmk_macro_param_2to2), label)
+
 #define ZM_IS_NODE_MATCH(a, b) (strcmp(a, b) == 0)
 #define IS_TAP_MODE(dev) ZM_IS_NODE_MATCH(dev, TAP_MODE)
 #define IS_PRESS_MODE(dev) ZM_IS_NODE_MATCH(dev, PRESS_MODE)
@@ -60,6 +65,11 @@ struct behavior_macro_config {
 #define IS_TAP_TIME(dev) ZM_IS_NODE_MATCH(dev, TAP_TIME)
 #define IS_WAIT_TIME(dev) ZM_IS_NODE_MATCH(dev, WAIT_TIME)
 #define IS_PAUSE(dev) ZM_IS_NODE_MATCH(dev, WAIT_REL)
+
+#define IS_P1TO1(dev) ZM_IS_NODE_MATCH(dev, P1TO1)
+#define IS_P1TO2(dev) ZM_IS_NODE_MATCH(dev, P1TO2)
+#define IS_P2TO1(dev) ZM_IS_NODE_MATCH(dev, P2TO1)
+#define IS_P2TO2(dev) ZM_IS_NODE_MATCH(dev, P2TO2)
 
 static bool handle_control_binding(struct behavior_macro_trigger_state *state,
                                    const struct zmk_behavior_binding *binding) {
@@ -78,6 +88,18 @@ static bool handle_control_binding(struct behavior_macro_trigger_state *state,
     } else if (IS_WAIT_TIME(binding->behavior_dev)) {
         state->wait_ms = binding->param1;
         LOG_DBG("macro wait time set: %d", state->wait_ms);
+    } else if (IS_P1TO1(binding->behavior_dev)) {
+        state->param1_source = PARAM_SOURCE_MACRO_1ST;
+        LOG_DBG("macro param: 1to1");
+    } else if (IS_P1TO2(binding->behavior_dev)) {
+        state->param2_source = PARAM_SOURCE_MACRO_1ST;
+        LOG_DBG("macro param: 1to2");
+    } else if (IS_P2TO1(binding->behavior_dev)) {
+        state->param1_source = PARAM_SOURCE_MACRO_2ND;
+        LOG_DBG("macro param: 2to1");
+    } else if (IS_P2TO2(binding->behavior_dev)) {
+        state->param2_source = PARAM_SOURCE_MACRO_2ND;
+        LOG_DBG("macro param: 2to2");
     } else {
         return false;
     }
@@ -110,21 +132,47 @@ static int behavior_macro_init(const struct device *dev) {
     return 0;
 };
 
+static uint32_t select_param(enum param_source param_source, uint32_t source_binding,
+                             const struct zmk_behavior_binding *macro_binding) {
+    switch (param_source) {
+    case PARAM_SOURCE_MACRO_1ST:
+        return macro_binding->param1;
+    case PARAM_SOURCE_MACRO_2ND:
+        return macro_binding->param2;
+    default:
+        return source_binding;
+    }
+};
+
+static void replace_params(struct behavior_macro_trigger_state *state,
+                           struct zmk_behavior_binding *binding,
+                           const struct zmk_behavior_binding *macro_binding) {
+    binding->param1 = select_param(state->param1_source, binding->param1, macro_binding);
+    binding->param2 = select_param(state->param2_source, binding->param2, macro_binding);
+
+    state->param1_source = PARAM_SOURCE_BINDING;
+    state->param2_source = PARAM_SOURCE_BINDING;
+}
+
 static void queue_macro(uint32_t position, const struct zmk_behavior_binding bindings[],
-                        struct behavior_macro_trigger_state state) {
+                        struct behavior_macro_trigger_state state,
+                        const struct zmk_behavior_binding *macro_binding) {
     LOG_DBG("Iterating macro bindings - starting: %d, count: %d", state.start_index, state.count);
     for (int i = state.start_index; i < state.start_index + state.count; i++) {
         if (!handle_control_binding(&state, &bindings[i])) {
+            struct zmk_behavior_binding binding = bindings[i];
+            replace_params(&state, &binding, macro_binding);
+
             switch (state.mode) {
             case MACRO_MODE_TAP:
-                zmk_behavior_queue_add(position, bindings[i], true, state.tap_ms);
-                zmk_behavior_queue_add(position, bindings[i], false, state.wait_ms);
+                zmk_behavior_queue_add(position, binding, true, state.tap_ms);
+                zmk_behavior_queue_add(position, binding, false, state.wait_ms);
                 break;
             case MACRO_MODE_PRESS:
-                zmk_behavior_queue_add(position, bindings[i], true, state.wait_ms);
+                zmk_behavior_queue_add(position, binding, true, state.wait_ms);
                 break;
             case MACRO_MODE_RELEASE:
-                zmk_behavior_queue_add(position, bindings[i], false, state.wait_ms);
+                zmk_behavior_queue_add(position, binding, false, state.wait_ms);
                 break;
             default:
                 LOG_ERR("Unknown macro mode: %d", state.mode);
@@ -145,7 +193,7 @@ static int on_macro_binding_pressed(struct zmk_behavior_binding *binding,
                                                          .start_index = 0,
                                                          .count = state->press_bindings_count};
 
-    queue_macro(event.position, cfg->bindings, trigger_state);
+    queue_macro(event.position, cfg->bindings, trigger_state, binding);
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -156,7 +204,7 @@ static int on_macro_binding_released(struct zmk_behavior_binding *binding,
     const struct behavior_macro_config *cfg = dev->config;
     struct behavior_macro_state *state = dev->data;
 
-    queue_macro(event.position, cfg->bindings, state->release_state);
+    queue_macro(event.position, cfg->bindings, state->release_state, binding);
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -166,22 +214,20 @@ static const struct behavior_driver_api behavior_macro_driver_api = {
     .binding_released = on_macro_binding_released,
 };
 
-#define BINDING_WITH_COMMA(idx, drv_inst) ZMK_KEYMAP_EXTRACT_BINDING(idx, DT_DRV_INST(drv_inst))
-
 #define TRANSFORMED_BEHAVIORS(n)                                                                   \
-    {LISTIFY(DT_PROP_LEN(DT_DRV_INST(n), bindings), BINDING_WITH_COMMA, (, ), n)},
+    {LISTIFY(DT_PROP_LEN(n, bindings), ZMK_KEYMAP_EXTRACT_BINDING, (, ), n)},
 
-#define MACRO_INST(n)                                                                              \
-    static struct behavior_macro_state behavior_macro_state_##n = {};                              \
-    static struct behavior_macro_config behavior_macro_config_##n = {                              \
-        .default_wait_ms = DT_INST_PROP_OR(n, wait_ms, CONFIG_ZMK_MACRO_DEFAULT_WAIT_MS),          \
-        .default_tap_ms = DT_INST_PROP_OR(n, tap_ms, CONFIG_ZMK_MACRO_DEFAULT_TAP_MS),             \
-        .count = DT_INST_PROP_LEN(n, bindings),                                                    \
-        .bindings = TRANSFORMED_BEHAVIORS(n)};                                                     \
-    DEVICE_DT_INST_DEFINE(n, behavior_macro_init, NULL, &behavior_macro_state_##n,                 \
-                          &behavior_macro_config_##n, APPLICATION,                                 \
-                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_macro_driver_api);
+#define MACRO_INST(inst)                                                                           \
+    static struct behavior_macro_state behavior_macro_state_##inst = {};                           \
+    static struct behavior_macro_config behavior_macro_config_##inst = {                           \
+        .default_wait_ms = DT_PROP_OR(inst, wait_ms, CONFIG_ZMK_MACRO_DEFAULT_WAIT_MS),            \
+        .default_tap_ms = DT_PROP_OR(inst, tap_ms, CONFIG_ZMK_MACRO_DEFAULT_TAP_MS),               \
+        .count = DT_PROP_LEN(inst, bindings),                                                      \
+        .bindings = TRANSFORMED_BEHAVIORS(inst)};                                                  \
+    DEVICE_DT_DEFINE(inst, behavior_macro_init, NULL, &behavior_macro_state_##inst,                \
+                     &behavior_macro_config_##inst, APPLICATION,                                   \
+                     CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_macro_driver_api);
 
-DT_INST_FOREACH_STATUS_OKAY(MACRO_INST)
-
-#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
+DT_FOREACH_STATUS_OKAY(zmk_behavior_macro, MACRO_INST)
+DT_FOREACH_STATUS_OKAY(zmk_behavior_macro_one_param, MACRO_INST)
+DT_FOREACH_STATUS_OKAY(zmk_behavior_macro_two_param, MACRO_INST)
