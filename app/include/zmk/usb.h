40cd8da743b10aee61e0396138354d062cd0e23e
Peter Johanson <peter@peterjohanson.com>
2022-04-13 03:20:35 +0000

    fix(usb): Split HID from core USB, logging fix.
    * Split core USB init from USB HID init.
    * Tweak logging to avoid "log loop" causing
      spurious buffer messages on startup.

--- app/include/zmk/usb.h
+++ app/include/zmk/usb.h
@@ -23,7 +23,3 @@ enum zmk_usb_conn_state zmk_usb_get_conn_state();
 
 static inline bool zmk_usb_is_powered() { return zmk_usb_get_conn_state() != ZMK_USB_CONN_NONE; }
 static inline bool zmk_usb_is_hid_ready() { return zmk_usb_get_conn_state() == ZMK_USB_CONN_HID; }
-
-#ifdef CONFIG_ZMK_USB
-int zmk_usb_hid_send_report(const uint8_t *report, size_t len);
-#endif /* CONFIG_ZMK_USB */
