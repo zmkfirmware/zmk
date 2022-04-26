9fd83a83637adf0f16de45e6387d4502c71dd422
Peter Johanson <peter@peterjohanson.com>
2022-04-26 05:00:46 +0000

    feat(display): Add new peripheral status/display
    * Add new API/status to track state of the
      peripheral connection to the central.
    * Add new peripheral status widget for displaying
      the current status of the connection to
      the central.

--- app/src/split/bluetooth/peripheral.c
+++ app/src/split/bluetooth/peripheral.c
@@ -30,6 +30,8 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+#include <zmk/event_manager.h>
+#include <zmk/events/split_peripheral_status_changed.h>
 #include <zmk/ble.h>
 #include <zmk/split/bluetooth/uuid.h>
 
@@ -39,9 +41,18 @@ static const struct bt_data zmk_ble_ad[] = {
                   ),
     BT_DATA_BYTES(BT_DATA_UUID128_ALL, ZMK_SPLIT_BT_SERVICE_UUID)};
 
+static bool is_connected = false;
+
 static int start_advertising() {
     return bt_le_adv_start(BT_LE_ADV_CONN, zmk_ble_ad, ARRAY_SIZE(zmk_ble_ad), NULL, 0);
 };
+
+static void connected(struct bt_conn *conn, uint8_t err) {
+    is_connected = (err == 0);
+
+    ZMK_EVENT_RAISE(new_zmk_split_peripheral_status_changed(
+        (struct zmk_split_peripheral_status_changed){.connected = is_connected}));
+}
 
 static void disconnected(struct bt_conn *conn, uint8_t reason) {
     char addr[BT_ADDR_LE_STR_LEN];
@@ -49,6 +60,11 @@ static void disconnected(struct bt_conn *conn, uint8_t reason) {
     bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
     LOG_DBG("Disconnected from %s (reason 0x%02x)", log_strdup(addr), reason);
+
+    is_connected = false;
+
+    ZMK_EVENT_RAISE(new_zmk_split_peripheral_status_changed(
+        (struct zmk_split_peripheral_status_changed){.connected = is_connected}));
 }
 
 static void security_changed(struct bt_conn *conn, bt_security_t level, enum bt_security_err err) {
@@ -73,10 +89,13 @@ static void le_param_updated(struct bt_conn *conn, uint16_t interval, uint16_t l
 }
 
 static struct bt_conn_cb conn_callbacks = {
+    .connected = connected,
     .disconnected = disconnected,
     .security_changed = security_changed,
     .le_param_updated = le_param_updated,
 };
+
+bool zmk_split_bt_peripheral_is_connected() { return is_connected; }
 
 static int zmk_peripheral_ble_init(const struct device *_arg) {
     int err = bt_enable(NULL);
