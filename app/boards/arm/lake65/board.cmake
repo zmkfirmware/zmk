b47457c1af9bf6a6c598dcad3e00f4fd19fa8b96
shuiguohuooo <97853321+shuihuohuooo@users.noreply.github.com>
2022-06-14 01:15:17 +0800

    Delete app/boards/arm/lake65 directory

--- app/boards/arm/lake65/board.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2021 Nick Winans
-# SPDX-License-Identifier: MIT
-
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
