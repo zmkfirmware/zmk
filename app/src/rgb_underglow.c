75750ec1c1ca4bc9f1dc359e9c4f1f6dc11f05d1
ReFil <31960031+ReFil@users.noreply.github.com>
2022-06-23 14:04:17 +0000

    Update app/src/rgb_underglow.c
    Co-authored-by: Pete Johanson <peter@peterjohanson.com>

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -272,8 +272,7 @@ static int zmk_rgb_underglow_init(const struct device *_arg) {
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_RGB_UNDERGLOW_AUTO_OFF_USB)
-    if (!zmk_usb_is_powered())
-        state.on = false;
+    state.on = zmk_usb_is_powered();
 #endif
 
     k_timer_start(&underglow_tick, K_NO_WAIT, K_MSEC(50));
