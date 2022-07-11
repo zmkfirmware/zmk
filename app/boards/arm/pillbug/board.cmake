3d7cfb3015f86d988f96449a9d4ce8ccb8f55797
Kyle McCreery <mccreery.kyle@gmail.com>
2022-07-11 14:36:35 -0400

    Initial commit of properly formatted OBE shield using Pillbug dev board.

---
+++ app/boards/arm/pillbug/board.cmake
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: MIT
+
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
