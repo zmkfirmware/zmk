9302133068b39363c8dde84490d7e958a4e52152
Chatblanc77 <107761260+Chatblanc77@users.noreply.github.com>
2022-09-09 13:10:13 +0700

    add encoder

--- app/include/zmk/split/bluetooth/service.h
+++ app/include/zmk/split/bluetooth/service.h
@@ -25,3 +25,4 @@ struct zmk_split_run_behavior_payload {
 int zmk_split_bt_position_pressed(uint8_t position);
 int zmk_split_bt_position_released(uint8_t position);
 int zmk_split_bt_sensor_triggered(uint8_t sensor_number, struct sensor_value value);
+
