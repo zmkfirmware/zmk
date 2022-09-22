92b75daa069e6b045a8f3763d86744f34c68d903
tmbutcher <tmbutcher@gmail.com>
2022-09-22 12:16:08 -0500

    Adding Adafruit Feather NRF52840 board

---
+++ app/boards/arm/adafruit_feather_nrf52840/board.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# board_runner_args(jlink "--device=nrf52" "--speed=4000")
+# board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+# board_runner_args(blackmagicprobe "--gdb-serial=/dev/ttyBmpGdb")
+# include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+# include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+# include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
