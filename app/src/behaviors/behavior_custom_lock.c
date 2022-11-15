776b118d951f99cc1e86073c18e1f8f7d110c6b6
Sophie Tyalie <dev@flowerpot.me>
2022-11-15 16:30:52 +0100

    CustomLock: add basic file structure
    This is the first commit introducing custom lock behavior. Currently
    does nothing, except existing and that one can reference it in the
    keyboard layout not dissimilar to e.g. `&none`.
    
    The code is also orientated along the latter for right now, ready to be
    extended.
    
    Signed-off-by: Sophie Tyalie <dev@flowerpot.me>

---
+++ app/src/behaviors/behavior_custom_lock.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_custom_lock
+
+#include <device.h>
+#include <drivers/behavior.h>
+#include <logging/log.h>
+
+#include <zmk/behavior.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_lock_init(const struct device *dev) { return 0; };
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    return 0;
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    return 0;
+}
+
+static const struct behavior_driver_api behavior_lock_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released
+};
+
+DEVICE_DT_INST_DEFINE(0, behavior_lock_init, NULL, NULL, NULL, APPLICATION,
+                      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_lock_driver_api);
+
+#endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
