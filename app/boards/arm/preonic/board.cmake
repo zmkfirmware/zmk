4bb241e689d372ab3d24b3aa24e7a4a57af66313
jeromeOlivier <jerome.olivier@icloud.com>
2022-12-09 12:00:44 +0000

    feat: preonic keyboard

---
+++ app/boards/arm/preonic/board.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2020 The ZMK Contributors
+#
+# SPDX-License-Identifier: MIT
+#
+
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(jlink "--device=STM32F303VC" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
