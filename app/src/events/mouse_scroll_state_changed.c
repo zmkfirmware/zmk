b2fefbb89cae8d9aacdc639c01d043ecf72b24cb
Merlinov <64584348+Merlinov@users.noreply.github.com>
2023-02-08 21:55:07 +0300

    Add files via upload

---
+++ app/src/events/mouse_scroll_state_changed.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <kernel.h>
+#include <zmk/events/mouse_scroll_state_changed.h>
+
+ZMK_EVENT_IMPL(zmk_mouse_scroll_state_changed);
