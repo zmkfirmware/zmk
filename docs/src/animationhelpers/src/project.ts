35549c45c948a88c504a544cf378939cb9455cd3
Kurtis Lew <kurtis.a.lew@gmail.com>
2023-02-10 09:00:30 -0800

    feat(docs): Initialize Motion Canvas Project

---
+++ docs/src/animationhelpers/src/project.ts
@@ -0,0 +1,8 @@
+import {makeProject} from '@motion-canvas/core/lib';
+
+import example from './scenes/example?scene';
+
+export default makeProject({
+  scenes: [example],
+  background: '#141414',
+});
