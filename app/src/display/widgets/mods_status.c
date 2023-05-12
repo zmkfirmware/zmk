20131e4779c6273ba088b14a587a2ece2371ab06
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-05-12 14:28:01 -0700

    feat(display): Add config for modifier chars

--- app/src/display/widgets/mods_status.c
+++ app/src/display/widgets/mods_status.c
@@ -14,6 +14,12 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/endpoints.h>
 #include <zmk/hid.h>
 
+#define MOD_CHARS CONFIG_ZMK_WIDGET_MODS_STATUS_CHARACTERS
+#define MOD_CHARS_LEN (sizeof(MOD_CHARS) - 1)
+
+BUILD_ASSERT(MOD_CHARS_LEN == 4,
+             "ERROR: CONFIG_ZMK_WIDGET_MODS_STATUS_CHARACTERS should have exactly 4 characters");
+
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct mods_status_state {
@@ -29,13 +35,13 @@ void set_mods_symbol(lv_obj_t *label, struct mods_status_state state) {
 
     LOG_DBG("mods changed to %i", state.mods);
     if (state.mods & (MOD_LCTL | MOD_RCTL))
-        strcat(text, "C");
+        strncat(text, &MOD_CHARS[0], 1);
     if (state.mods & (MOD_LSFT | MOD_RSFT))
-        strcat(text, "S");
+        strncat(text, &MOD_CHARS[1], 1);
     if (state.mods & (MOD_LALT | MOD_RALT))
-        strcat(text, "A");
+        strncat(text, &MOD_CHARS[2], 1);
     if (state.mods & (MOD_LGUI | MOD_RGUI))
-        strcat(text, "G");
+        strncat(text, &MOD_CHARS[3], 1);
 
     lv_label_set_text(label, text);
     lv_obj_align(label, LV_ALIGN_BOTTOM_RIGHT, -1, 0);
