7ce38da9c5faf7a64f3c8ffd89b66d71518548bd
Joel Spadin <joelspadin@gmail.com>
2023-08-05 23:29:48 -0500

    feat(behaviors): Add a send string behavior
    This adds the following:
    
    - A character map driver API, which maps Unicode code points to behavior
      bindings.
    
    - A zmk,character-map driver which implements this API. This driver is
      designed for ROM efficiency, so it sends every value defined in the
      map to one behavior and passes any code point not in the map through
      to another. (A more flexible implementation that allows for a unique
      behavior binding per character could be added later if necessary.)
    
    - A zmk,send-string behavior, which users can define and bind to their
      keymaps to send strings.
    
    - A zmk_send_string() function, which queues the necessary behaviors
      to type a UTF-8 string. This is separated from the send string
      behavior since it could be used for other features such as Unicode
      key sequences, behaviors that print dynamic messages, etc.

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -35,6 +35,7 @@ module.exports = {
       "behaviors/tap-dance",
       "behaviors/caps-word",
       "behaviors/key-repeat",
+      "behaviors/send-string",
       "behaviors/sensor-rotate",
       "behaviors/mouse-emulation",
       "behaviors/reset",
