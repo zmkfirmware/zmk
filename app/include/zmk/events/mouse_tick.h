9a68b376b45fbc4d2c22789fe05b0ec831667f5f
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-10-15 23:27:45 -0700

    feat(mouse): Split move/scroll & x/y acceleration

--- app/include/zmk/events/mouse_tick.h
+++ app/include/zmk/events/mouse_tick.h
@@ -17,7 +17,7 @@ struct zmk_mouse_tick {
     struct vector2d max_scroll;
     struct mouse_config move_config;
     struct mouse_config scroll_config;
-    int64_t *start_time;
+    struct mouse_times start_times;
     int64_t timestamp;
 };
 
@@ -27,13 +27,13 @@ static inline struct zmk_mouse_tick_event *zmk_mouse_tick(struct vector2d max_mo
                                                           struct vector2d max_scroll,
                                                           struct mouse_config move_config,
                                                           struct mouse_config scroll_config,
-                                                          int64_t *movement_start) {
+                                                          struct mouse_times movement_start) {
     return new_zmk_mouse_tick((struct zmk_mouse_tick){
         .max_move = max_move,
         .max_scroll = max_scroll,
         .move_config = move_config,
         .scroll_config = scroll_config,
-        .start_time = movement_start,
+        .start_times = movement_start,
         .timestamp = k_uptime_get(),
     });
 }
