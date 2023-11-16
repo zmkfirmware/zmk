f8c1f470dc93d32892efab90966a051e1a1d5b97
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-11-15 21:11:41 -0800

    feat(mouse): Add mouse move and scroll support
    * Use Zephyr input subsystem for all pointers.
    * Input processors for modifying events, e.g. scaling, swapping
      codes, temporary (mouse) layers, etc.
    * Mouse move/scroll behaviors.
    * Infrastructure in place for physical pointer input devices.
    
    Co-authored-by: Alexander Krikun <krikun98@gmail.com>
    Co-authored-by: Robert U <urob@users.noreply.github.com>
    Co-authored-by: Shawn Meier <ftc@users.noreply.github.com>
    Co-authored-by: Chris Andreae <chris@andreae.gen.nz>

--- app/include/zmk/usb_hid.h
+++ app/include/zmk/usb_hid.h
@@ -10,7 +10,7 @@
 
 int zmk_usb_hid_send_keyboard_report(void);
 int zmk_usb_hid_send_consumer_report(void);
-#if IS_ENABLED(CONFIG_ZMK_MOUSE)
+#if IS_ENABLED(CONFIG_ZMK_POINTING)
 int zmk_usb_hid_send_mouse_report(void);
-#endif // IS_ENABLED(CONFIG_ZMK_MOUSE)
+#endif // IS_ENABLED(CONFIG_ZMK_POINTING)
 void zmk_usb_hid_set_protocol(uint8_t protocol);
