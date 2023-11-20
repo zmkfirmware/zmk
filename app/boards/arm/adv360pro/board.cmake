a924216bc1bc1c79f1240c448534051144ab5ed7
ReFil <31960031+ReFil@users.noreply.github.com>
2023-11-20 20:37:42 +0000

    Address review comments

--- app/boards/arm/adv360pro/board.cmake
+++ app/boards/arm/adv360pro/board.cmake
@@ -1,9 +1,8 @@
 #
-# Copyright (c) 2022 The ZMK Contributors
+# Copyright (c) 2023 The ZMK Contributors
 # SPDX-License-Identifier: MIT
 #
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-#include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
