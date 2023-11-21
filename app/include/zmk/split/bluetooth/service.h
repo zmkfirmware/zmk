ff7e0b7e481facf068be9d7f50b20445f042d11e
ReFil <31960031+ReFil@users.noreply.github.com>
2023-11-21 09:40:56 +0000

    feat(split): central to peripheral communication

--- app/include/zmk/split/bluetooth/service.h
+++ app/include/zmk/split/bluetooth/service.h
@@ -10,6 +10,20 @@
 #include <zmk/sensors.h>
 
 #define ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN 9
+#define ZMK_SPLIT_DATA_XFER_MAX_LEN 16
+
+enum data_tag {
+    // RGB state
+    DATA_TAG_RGB_STATE,
+    // Backlight state
+    DATA_TAG_BACKLIGHT_STATE,
+    // HID indicators state
+    DATA_TAG_HID_INDICATORS_STATE,
+    // Keymap state
+    DATA_TAG_KEYMAP_STATE,
+    // Start of custom tags
+    DATA_TAG_CUSTOM_START,
+};
 
 struct sensor_event {
     uint8_t sensor_index;
@@ -28,6 +42,12 @@ struct zmk_split_run_behavior_data {
 struct zmk_split_run_behavior_payload {
     struct zmk_split_run_behavior_data data;
     char behavior_dev[ZMK_SPLIT_RUN_BEHAVIOR_DEV_LEN];
+} __packed;
+
+struct zmk_split_data_xfer_data {
+    enum data_tag data_tag;
+    uint8_t data_size;
+    uint8_t data[ZMK_SPLIT_DATA_XFER_MAX_LEN];
 } __packed;
 
 int zmk_split_bt_position_pressed(uint8_t position);
