3968d9febd434123d7ac3296a934e5913141277d
Peter Johanson <peter@peterjohanson.com>
2023-12-04 13:55:42 -0800

    feat(boards): Make `west flash` work with new UF2 runner.

--- app/boards/arm/pillbug/board.cmake
+++ app/boards/arm/pillbug/board.cmake
@@ -2,5 +2,6 @@
 
 set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
