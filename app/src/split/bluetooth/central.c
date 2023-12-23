f0b45b7df96d452b6bdb8faa1b78418e3271429c
Xudong Zheng <7pkvm5aw@slicealias.com>
2023-12-23 17:42:31 -0500

    refactor(split): move central sensor handling out of Bluetooth directory

--- app/src/split/bluetooth/central.c
+++ app/src/split/bluetooth/central.c
@@ -237,19 +237,6 @@ int confirm_peripheral_slot_conn(struct bt_conn *conn) {
 }
 
 #if ZMK_KEYMAP_HAS_SENSORS
-K_MSGQ_DEFINE(peripheral_sensor_event_msgq, sizeof(struct zmk_sensor_event),
-              CONFIG_ZMK_SPLIT_BLE_CENTRAL_POSITION_QUEUE_SIZE, 4);
-
-void peripheral_sensor_event_work_callback(struct k_work *work) {
-    struct zmk_sensor_event ev;
-    while (k_msgq_get(&peripheral_sensor_event_msgq, &ev, K_NO_WAIT) == 0) {
-        LOG_DBG("Trigger sensor change for %d", ev.sensor_index);
-        raise_zmk_sensor_event(ev);
-    }
-}
-
-K_WORK_DEFINE(peripheral_sensor_event_work, peripheral_sensor_event_work_callback);
-
 static uint8_t split_central_sensor_notify_func(struct bt_conn *conn,
                                                 struct bt_gatt_subscribe_params *params,
                                                 const void *data, uint16_t length) {
@@ -275,8 +262,7 @@ static uint8_t split_central_sensor_notify_func(struct bt_conn *conn,
 
     memcpy(ev.channel_data, sensor_event.channel_data,
            sizeof(struct zmk_sensor_channel_data) * sensor_event.channel_data_size);
-    k_msgq_put(&peripheral_sensor_event_msgq, &ev, K_NO_WAIT);
-    k_work_submit(&peripheral_sensor_event_work);
+    zmk_sensor_event_handle(&ev);
 
     return BT_GATT_ITER_CONTINUE;
 }
