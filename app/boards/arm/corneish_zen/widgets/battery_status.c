6f8d080b6a3bf54db58a75ff77288be13119273c
Justin Ridgewell <justin@ridgewell.name>
2024-01-05 15:02:06 -0500

    fix: Use `zmk_battery_state_of_charge` in battery widgets

--- app/boards/arm/corneish_zen/widgets/battery_status.c
+++ app/boards/arm/corneish_zen/widgets/battery_status.c
@@ -67,7 +67,7 @@ void battery_status_update_cb(struct battery_status_state state) {
 
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
     return (struct battery_status_state) {
-        .level = bt_bas_get_battery_level(),
+        .level = zmk_battery_state_of_charge(),
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
