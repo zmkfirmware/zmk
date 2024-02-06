9b9ddd53bdbfffbacf50561f79fe6d4385fde64d
aggstam <aggelosstam13@gmail.com>
2024-02-06 13:36:20 +0200

    feat(display): label widget updated licence year and removed text length limit

--- app/src/display/widgets/label.c
+++ app/src/display/widgets/label.c
@@ -1,21 +1,13 @@
 /*
- * Copyright (c) 2020 The ZMK Contributors
+ * Copyright (c) 2024 The ZMK Contributors
  *
  * SPDX-License-Identifier: MIT
  */
 
 #include <zephyr/kernel.h>
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
 #include <zmk/display.h>
 #include <zmk/display/widgets/label.h>
-
-#define WIDGET_LABEL_TEXT_MAX 7
-
-BUILD_ASSERT(sizeof(CONFIG_ZMK_WIDGET_LABEL_TEXT) - 1 <= WIDGET_LABEL_TEXT_MAX,
-             "ERROR: Widget label text length is too long. Max length: 7");
 
 int zmk_widget_label_init(struct zmk_widget_label *widget, lv_obj_t *parent) {
     widget->obj = lv_label_create(parent);
