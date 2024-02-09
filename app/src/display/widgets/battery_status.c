dca726de3d19180a202a2683c56e858d386ebed0
zhiayang <gpg@tsumaran.ai>
2024-02-09 01:29:19 -0500

    simplify code

--- app/src/display/widgets/battery_status.c
+++ app/src/display/widgets/battery_status.c
@@ -64,15 +64,9 @@ void battery_status_update_cb(struct battery_status_state state) {
 
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
     const struct zmk_battery_state_changed *ev = as_zmk_battery_state_changed(eh);
-    uint8_t soc;
-    if (ev != NULL) {
-        soc = ev->state_of_charge;
-    } else {
-        soc = zmk_battery_state_of_charge();
-    }
 
     return (struct battery_status_state) {
-        .level = soc,
+        .level = (ev != NULL) ? ev->state_of_charge : zmk_battery_state_of_charge(),
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
