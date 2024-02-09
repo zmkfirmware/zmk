4443164e3fa0358cbb7e26342795a21b235f862a
zhiayang <gpg@tsumaran.ai>
2024-02-09 00:49:56 -0500

    fix initial display of battery status on displays, and also fix a null deref

--- app/boards/shields/nice_view/widgets/status.c
+++ app/boards/shields/nice_view/widgets/status.c
@@ -210,8 +210,16 @@ static void battery_status_update_cb(struct battery_status_state state) {
 }
 
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-    return (struct battery_status_state) {
-        .level = zmk_battery_state_of_charge(),
+    const struct zmk_battery_state_changed *ev = as_zmk_battery_state_changed(eh);
+    uint8_t soc;
+    if (ev != NULL) {
+        soc = ev->state_of_charge;
+    } else {
+        soc = zmk_battery_state_of_charge();
+    }
+
+    return (struct battery_status_state){
+        .level = soc,
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
