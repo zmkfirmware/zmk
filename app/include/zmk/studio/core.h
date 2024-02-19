49b2f9b95349a83d14ff2cbd373f29d9e2519007
Peter Johanson <peter@peterjohanson.com>
2024-02-19 08:48:20 +0000

    feat(studio): Initial RPC infrastructure and subsystems.
    * UART and BLE/GATT transports for a protobuf encoded RPC
      request/response protocol.
    * Custom framing protocol is used to frame a give message.
    * Requests/responses are divided into major "subsystems" which
      handle requests and create response messages.
    * Notification support, including mapping local events to RPC
      notifications by a given subsystem.
    * Meta responses for "no response" and "unlock needed".
    * Initial basic lock state support in a new core section, and allow specifying
      if a given RPC callback requires unlocked state or not.
    * Add behavior subsystem with full metadata support and examples of
      using callback to serialize a repeated field without extra stack space needed.

---
+++ app/include/zmk/studio/core.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include <zmk/event_manager.h>
+
+enum zmk_studio_core_lock_state {
+    ZMK_STUDIO_CORE_LOCK_STATE_LOCKED = 0,
+    ZMK_STUDIO_CORE_LOCK_STATE_UNLOCKING = 1,
+    ZMK_STUDIO_CORE_LOCK_STATE_UNLOCKED = 2,
+};
+
+struct zmk_studio_core_lock_state_changed {
+    enum zmk_studio_core_lock_state state;
+};
+
+struct zmk_studio_core_unlock_requested {};
+
+ZMK_EVENT_DECLARE(zmk_studio_core_lock_state_changed);
+ZMK_EVENT_DECLARE(zmk_studio_core_unlock_requested);
+
+enum zmk_studio_core_lock_state zmk_studio_core_get_lock_state(void);
+
+void zmk_studio_core_unlock();
+void zmk_studio_core_lock();
+void zmk_studio_core_initiate_unlock();
+void zmk_studio_core_complete_unlock();
+
+void zmk_studio_core_reschedule_lock_timeout();
