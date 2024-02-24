a77288f5274b8cd64aed5dc03bb6c03ec205240f
Joel Spadin <joelspadin@gmail.com>
2024-02-24 12:14:37 -0600

    fix: Update settings reset for Zephyr 3.5

--- app/src/settings/reset_settings_on_start.c
+++ app/src/settings/reset_settings_on_start.c
@@ -4,16 +4,10 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <zephyr/device.h>
 #include <zephyr/init.h>
 
 #include <zmk/settings.h>
 
-static int reset_settings_init(const struct device *dev) {
-    ARG_UNUSED(dev);
-    return zmk_settings_erase();
-}
-
 // Reset after the kernel is initialized but before any application code to
 // ensure settings are cleared before anything tries to use them.
-SYS_INIT(reset_settings_init, POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(zmk_settings_erase, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
