c5cca5b34f53263892257460acf681ffa490a240
Peter Johanson <peter@peterjohanson.com>
2024-04-24 18:14:02 -0700

    feat: Add keyboard physical layout system.
    * Add bindings to allow creating multiple physical layouts that specify
      their key's physical attributes, and the matching matrix transform
      and dependant kscan to use.
    * Synthesize a basic physical layout if none specified, for backwards
      compatibility.
    * Update matrix transform API to explicitly pass in the selected transform
      to the API instead of using a fixed chosen transform.
    * Move kscan subscription and handling into the physical layout code, so
      that selecting a different physical layout at runtime can also use the
      correct kscan instance.
    * Add `physical_layouts.dtsi` file to include so you can use the
      pre-configured `&key_physical_attrs` for adding you layout keys.

--- app/include/zmk/matrix_transform.h
+++ app/include/zmk/matrix_transform.h
@@ -6,4 +6,16 @@
 
 #pragma once
 
-int32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column);
+#include <zephyr/sys/util.h>
+
+typedef const struct zmk_matrix_transform *zmk_matrix_transform_t;
+
+#define ZMK_MATRIX_TRANSFORM_DEFAULT_EXTERN()                                                      \
+    extern const struct zmk_matrix_transform zmk_matrix_transform_default
+#define ZMK_MATRIX_TRANSFORM_EXTERN(node_id)                                                       \
+    extern const struct zmk_matrix_transform _CONCAT(zmk_matrix_transform_, node_id)
+
+#define ZMK_MATRIX_TRANSFORM_T_FOR_NODE(node_id) &_CONCAT(zmk_matrix_transform_, node_id)
+
+int32_t zmk_matrix_transform_row_column_to_position(zmk_matrix_transform_t mt, uint32_t row,
+                                                    uint32_t column);
