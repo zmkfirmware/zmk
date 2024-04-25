da0f18cf62a00775306b0568b5d119dfec07abaa
Peter Johanson <peter@peterjohanson.com>
2024-04-24 18:14:02 -0700

    feat: Add keyboard physical layout system.
    * Add bindings to allow creating multiple physical layouts that specify
      their key's physical attributes, and the matching matrix transform
      and dependant kscan to use.
    * Synthesize a basic physical layout if none specified, for backwards
      compatibility.
    * Update matrix transform API to explicitly pass in the selected transform
      to the API instead of using a fixed chosen transform.
    * Move kscan subscription and handling into the physical layout code, so
      that selecting a different physical layout at runtime can also use the
      correct kscan instance.
    * Add `physical_layouts.dtsi` file to include so you can use the
      pre-configured `&key_physical_attrs` for adding you layout keys.

--- app/include/zmk/matrix.h
+++ app/include/zmk/matrix.h
@@ -9,14 +9,20 @@
 #include <zephyr/devicetree.h>
 
 #define ZMK_MATRIX_NODE_ID DT_CHOSEN(zmk_kscan)
+#define ZMK_MATRIX_HAS_TRANSFORM DT_HAS_CHOSEN(zmk_matrix_transform)
 
-#if DT_HAS_CHOSEN(zmk_matrix_transform)
+#if DT_HAS_COMPAT_STATUS_OKAY(zmk_physical_layout)
+
+#define ZMK_PHYSICAL_LAYOUT_BYTE_ARRAY(node_id)                                                    \
+    uint8_t _CONCAT(prop_, node_id)[DT_PROP_LEN(DT_PHANDLE(node_id, transform), map)];
+
+#define ZMK_KEYMAP_LEN                                                                             \
+    sizeof(union {DT_FOREACH_STATUS_OKAY(zmk_physical_layout, ZMK_PHYSICAL_LAYOUT_BYTE_ARRAY)})
+
+#elif ZMK_MATRIX_HAS_TRANSFORM
 
 #define ZMK_KEYMAP_TRANSFORM_NODE DT_CHOSEN(zmk_matrix_transform)
 #define ZMK_KEYMAP_LEN DT_PROP_LEN(ZMK_KEYMAP_TRANSFORM_NODE, map)
-
-#define ZMK_MATRIX_ROWS DT_PROP(ZMK_KEYMAP_TRANSFORM_NODE, rows)
-#define ZMK_MATRIX_COLS DT_PROP(ZMK_KEYMAP_TRANSFORM_NODE, columns)
 
 #else /* DT_HAS_CHOSEN(zmk_matrix_transform) */
 
--- app/src/main.c
+++ app/src/main.c
@@ -12,17 +12,10 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#include <zmk/matrix.h>
-#include <zmk/kscan.h>
 #include <zmk/display.h>
-#include <drivers/ext_power.h>
 
 int main(void) {
     LOG_INF("Welcome to ZMK!\n");
-
-    if (zmk_kscan_init(DEVICE_DT_GET(ZMK_MATRIX_NODE_ID)) != 0) {
-        return -ENOTSUP;
-    }
 
 #ifdef CONFIG_ZMK_DISPLAY
     zmk_display_init();
