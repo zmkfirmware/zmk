2ee76be6fee671042d9740ac0271eb69e2558165
German Gutierrez <GermanG@users.noreply.github.com>
2024-05-13 23:43:35 +0200

    fix(soft_off): central waits 100ms in split if hold_time enabled

--- app/src/behaviors/behavior_soft_off.c
+++ app/src/behaviors/behavior_soft_off.c
@@ -57,6 +57,9 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
         uint32_t hold_time = k_uptime_get() - data->press_start;
 
         if (hold_time > config->hold_time_ms) {
+            if (IS_ENABLED(CONFIG_ZMK_SPLIT) && IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)) {
+                k_sleep(K_MSEC(100));
+            }
             zmk_pm_soft_off();
         } else {
             LOG_INF("Not triggering soft off: held for %d and hold time is %d", hold_time,
