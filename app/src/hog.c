7c53ad56b81f57c773d67b11222877ee21d1a694
Peter Johanson <peter@peterjohanson.com>
2024-06-28 17:40:49 -0600

    fix: BLE refactor mouse keys fix.

--- app/src/hog.c
+++ app/src/hog.c
@@ -336,7 +336,7 @@ K_MSGQ_DEFINE(zmk_hog_mouse_msgq, sizeof(struct zmk_hid_mouse_report_body),
 void send_mouse_report_callback(struct k_work *work) {
     struct zmk_hid_mouse_report_body report;
     while (k_msgq_get(&zmk_hog_mouse_msgq, &report, K_NO_WAIT) == 0) {
-        struct bt_conn *conn = destination_connection();
+        struct bt_conn *conn = zmk_ble_active_profile_conn();
         if (conn == NULL) {
             return;
         }
