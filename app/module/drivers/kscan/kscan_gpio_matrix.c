a2c2cc6a1c6796fd1ffbc2ad5a5140abd40884bb
Peter Johanson <peter@peterjohanson.com>
2024-07-05 00:34:03 -0600

    fix: kscan driver device runtime PM include fix.

--- app/module/drivers/kscan/kscan_gpio_matrix.c
+++ app/module/drivers/kscan/kscan_gpio_matrix.c
@@ -11,6 +11,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/kscan.h>
 #include <zephyr/pm/device.h>
+#include <zephyr/pm/device_runtime.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/__assert.h>
