0c3a642d9569b97e947f70b8e26fd44e0737a667
Pedro Huerta <77652963+phuertay@users.noreply.github.com>
2024-09-18 10:37:25 -0600

    Update hog.h

--- app/include/zmk/hog.h
+++ app/include/zmk/hog.h
@@ -11,6 +11,7 @@
 
 int zmk_hog_send_keyboard_report(struct zmk_hid_keyboard_report_body *body);
 int zmk_hog_send_consumer_report(struct zmk_hid_consumer_report_body *body);
+int zmk_hog_send_plover_report(struct zmk_hid_plover_report_body *body);
 
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
 int zmk_hog_send_mouse_report(struct zmk_hid_mouse_report_body *body);
