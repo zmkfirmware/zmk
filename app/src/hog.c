52d1ed9c88b37c7e6ca46a14834d14e4374d6bda
Pedro Huerta <77652963+phuertay@users.noreply.github.com>
2024-09-18 11:02:25 -0600

    Update hog.c

--- app/src/hog.c
+++ app/src/hog.c
@@ -72,6 +72,16 @@ static struct hids_report consumer_input = {
     .type = HIDS_INPUT,
 };
 
+
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
+
+static struct hids_report plover_input = {
+    .id = 0x50,
+    .type = HIDS_INPUT,
+};
+
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
+
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
 
 static struct hids_report mouse_input = {
@@ -155,8 +165,19 @@ static ssize_t read_hids_consumer_input_report(struct bt_conn *conn,
                              sizeof(struct zmk_hid_consumer_report_body));
 }
 
-#if IS_ENABLED(CONFIG_ZMK_POINTING)
 
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
+static ssize_t read_hids_plover_input_report(struct bt_conn *conn,
+                                             const struct bt_gatt_attr *attr, void *buf,
+                                             uint16_t len, uint16_t offset) {
+    struct zmk_hid_plover_report_body *report_body = &zmk_hid_get_plover_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_plover_report_body));
+}
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
+
+
+#if IS_ENABLED(CONFIG_ZMK_POINTING)
 static ssize_t read_hids_mouse_input_report(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                             void *buf, uint16_t len, uint16_t offset) {
     struct zmk_hid_mouse_report_body *report_body = &zmk_hid_get_mouse_report()->body;
@@ -250,6 +271,14 @@ BT_GATT_SERVICE_DEFINE(
     BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),
     BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,
                        NULL, &consumer_input),
+
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
+    BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
+                           BT_GATT_PERM_READ_ENCRYPT, read_hids_plover_input_report, NULL, NULL),
+    BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),
+    BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,
+                       NULL, &plover_input),
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
 
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
     BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
@@ -388,6 +417,63 @@ int zmk_hog_send_consumer_report(struct zmk_hid_consumer_report_body *report) {
 
     return 0;
 };
+
+#if IS_ENABLED(CONFIG_ZMK_PLOVER_HID)
+
+K_MSGQ_DEFINE(zmk_hog_plover_msgq, sizeof(struct zmk_hid_plover_report_body),
+              CONFIG_ZMK_BLE_PLOVER_HID_REPORT_QUEUE_SIZE, 4);
+
+void send_plover_report_callback(struct k_work *work) {
+    struct zmk_hid_plover_report_body report;
+
+    while (k_msgq_get(&zmk_hog_plover_msgq, &report, K_NO_WAIT) == 0) {
+        struct bt_conn *conn = destination_connection();
+        if (conn == NULL) {
+            return;
+        }
+
+        struct bt_gatt_notify_params notify_params = {
+            // FIXME: Try to understand this offset calculation, because I just brute-forced it
+            // until it worked.
+            // 13 seems to be working, but might be wrong
+            // or 14
+            .attr = &hog_svc.attrs[13],
+            .data = &report,
+            .len = sizeof(report),
+        };
+
+        int err = bt_gatt_notify_cb(conn, &notify_params);
+        if (err) {
+            LOG_DBG("Error notifying %d", err);
+        }
+        bt_conn_unref(conn);
+    }
+}
+
+K_WORK_DEFINE(hog_plover_work, send_plover_report_callback);
+
+int zmk_hog_send_plover_report(struct zmk_hid_plover_report_body *report) {
+    int err = k_msgq_put(&zmk_hog_plover_msgq, report, K_MSEC(100));
+    if (err) {
+        switch (err) {
+        case -EAGAIN: {
+            LOG_WRN("Plover message queue full, popping first message and queueing again");
+            struct zmk_hid_plover_report_body discarded_report;
+            k_msgq_get(&zmk_hog_plover_msgq, &discarded_report, K_NO_WAIT);
+            return zmk_hog_send_plover_report(report);
+        }
+        default:
+            LOG_WRN("Failed to queue plover report to send (%d)", err);
+            return err;
+        }
+    }
+
+    k_work_submit_to_queue(&hog_work_q, &hog_plover_work);
+
+    return 0;
+}
+
+#endif /* IS_ENABLED(CONFIG_ZMK_PLOVER_HID) */
 
 #if IS_ENABLED(CONFIG_ZMK_POINTING)
 
