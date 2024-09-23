691598206460deebc94f956e3da152341d8c46a4
William Lindholm <william_lindholm@icloud.com>
2024-09-23 17:59:08 +0200

    feat: Add support for switching to previously used bluetooth profile

--- app/include/dt-bindings/zmk/bt.h
+++ app/include/dt-bindings/zmk/bt.h
@@ -7,9 +7,10 @@
 #define BT_CLR_CMD 0
 #define BT_NXT_CMD 1
 #define BT_PRV_CMD 2
-#define BT_SEL_CMD 3
-#define BT_CLR_ALL_CMD 4
-#define BT_DISC_CMD 5
+#define BT_PRV_DEV_CMD 3
+#define BT_SEL_CMD 4
+#define BT_CLR_ALL_CMD 5
+#define BT_DISC_CMD 6
 
 /*
 Note: Some future commands will include additional parameters, so we
@@ -19,6 +20,7 @@ defines these aliases up front.
 #define BT_CLR BT_CLR_CMD 0
 #define BT_NXT BT_NXT_CMD 0
 #define BT_PRV BT_PRV_CMD 0
+#define BT_PRV_DEV BT_PRV_DEV_CMD 0
 #define BT_SEL BT_SEL_CMD
 #define BT_CLR_ALL BT_CLR_ALL_CMD 0
 #define BT_DISC BT_DISC_CMD
