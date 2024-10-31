0090d3f5d8e60ff771c9f665841e32ba1b65911d
RimTanaka <42739802+RimTanaka@users.noreply.github.com>
2024-10-31 17:28:36 +0300

    Add files via upload

--- app/src/events/mouse_move_state_changed.c
+++ app/src/events/mouse_move_state_changed.c
@@ -5,6 +5,6 @@
  */
 
 #include <kernel.h>
-#include <zmk/events/mouse_button_state_changed.h>
+#include <zmk/events/mouse_move_state_changed.h>
 
-ZMK_EVENT_IMPL(zmk_mouse_button_state_changed);
+ZMK_EVENT_IMPL(zmk_mouse_move_state_changed);
--- app/src/events/mouse_scroll_state_changed.c
+++ app/src/events/mouse_scroll_state_changed.c
@@ -5,6 +5,6 @@
  */
 
 #include <kernel.h>
-#include <zmk/events/mouse_button_state_changed.h>
+#include <zmk/events/mouse_scroll_state_changed.h>
 
-ZMK_EVENT_IMPL(zmk_mouse_button_state_changed);
+ZMK_EVENT_IMPL(zmk_mouse_scroll_state_changed);
