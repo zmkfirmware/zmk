e7344de0c2ae3d36ea0f2374ff4616004259a97e
zhiayang <gpg@tsumaran.ai>
2024-11-17 21:55:25 +0800

    change 7.1 to 7.2 since people are selling that one

--- app/boards/arm/mikoto/revision.cmake
+++ app/boards/arm/mikoto/revision.cmake
@@ -3,5 +3,5 @@ board_check_revision(FORMAT MAJOR.MINOR.PATCH
                      VALID_REVISIONS
                      5.20.0         # first public release
                      6.1.0  6.3.0   # incompatible pinout change from v5+
-                     7.1.0          # addition of MAX17048; compatible pinout with v6+
+                     7.2.0          # addition of MAX17048; compatible pinout with v6+
 )
