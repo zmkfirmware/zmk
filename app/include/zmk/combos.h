f0a77b888ac482a863386ced08e04660ddacb026
Pete Johanson <peter@peterjohanson.com>
2025-01-01 16:14:53 -0700

    fix(combos): Properly report combos len with emply block (#2739)
    Handle the scenario where there is an empty combos block and return
    a zero combos length.

--- app/include/zmk/combos.h
+++ app/include/zmk/combos.h
@@ -8,9 +8,9 @@
 
 #include <zephyr/devicetree.h>
 
-#define ZMK_COMBOS_UTIL_ONE(n) 1
+#define ZMK_COMBOS_UTIL_ONE(n) +1
 
 #define ZMK_COMBOS_LEN                                                                             \
-    COND_CODE_1(                                                                                   \
-        DT_HAS_COMPAT_STATUS_OKAY(zmk_combos),                                                     \
-        (DT_FOREACH_CHILD_STATUS_OKAY_SEP(DT_INST(0, zmk_combos), ZMK_COMBOS_UTIL_ONE, (+))), (0))
+    COND_CODE_1(DT_HAS_COMPAT_STATUS_OKAY(zmk_combos),                                             \
+                (0 DT_FOREACH_CHILD_STATUS_OKAY(DT_INST(0, zmk_combos), ZMK_COMBOS_UTIL_ONE)),     \
+                (0))
