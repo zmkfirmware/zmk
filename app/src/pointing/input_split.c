2ed4f87f9ba3cfe1fc202c119cb8fcc30bb22d91
badjeff <badjeff@gmail.com>
2025-02-28 15:46:18 +0800

    Update app/src/pointing/input_split.c
    Co-authored-by: Pete Johanson <peter@peterjohanson.com>

--- app/src/pointing/input_split.c
+++ app/src/pointing/input_split.c
@@ -59,7 +59,7 @@ int zmk_input_split_report_peripheral_event(uint8_t reg, uint8_t type, uint16_t 
             int ret = zmk_input_processor_handle_event(processors_##n[i].dev, evt,                 \
                                                        processors_##n[i].param1,                   \
                                                        processors_##n[i].param2, NULL);            \
-            if (ret == ZMK_INPUT_PROC_CONTINUE) { continue; } else { return; }                     \
+            if (ret != ZMK_INPUT_PROC_CONTINUE) { return; }                     \
         }                                                                                          \
         zmk_split_bt_report_input(DT_INST_REG_ADDR(n), evt->type, evt->code, evt->value,           \
                                   evt->sync);                                                      \
