c238f933243dd1a1a0ffa9f885cfc598f350af26
Nick Conway <nick@conway.dev>
2025-03-14 10:26:11 -0400

    feat(behaviors): Turbo Key - refactor transform macro

--- app/src/behaviors/behavior_turbo_key.c
+++ app/src/behaviors/behavior_turbo_key.c
@@ -133,7 +133,7 @@ static const struct behavior_driver_api behavior_turbo_key_driver_api = {
 
 #define _TRANSFORM_ENTRY(idx, node)                                                                \
     {                                                                                              \
-        .behavior_dev = DT_LABEL(DT_INST_PHANDLE_BY_IDX(node, bindings, idx)),                     \
+        .behavior_dev = DT_PROP(DT_INST_PHANDLE_BY_IDX(node, bindings, idx), label),               \
         .param1 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param1), (0),       \
                               (DT_INST_PHA_BY_IDX(node, bindings, idx, param1))),                  \
         .param2 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param2), (0),       \
