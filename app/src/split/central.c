zmkfirmware/zmk

commit 147c340c6e8d377304acfdd64dc86cf83ebdfef2

Feature: Full-Duplex Wired Split (#2766)
refactor(split): Refactor split code for extension

Extract central/peripheral code to allow for plugging in alternate
transports, instead of tying all split logic to BT.

feat(split): Add full-duplex wired split support

* Depends on full-duplex hardware UART for communication.
* Supports all existing central commands/peripheral events, including
  sensors/inputs from peripherals.
* Only one wired split peripheral supported (for now)
* Relies on chosen `zmk,split-uart` referencing the UART device.

docs: Add wired split config docs.

Migrate split to its own dedicated config file, and add details
on wired split config.

Co-authored-by: Nicolas Munnich <98408764+Nick-Munnich@users.noreply.github.com>

fix: Properly override stack size on RP2040

Move the system work queue stack size override on RP2040 ouf of
a `ZMK_BLE` conditional so it is properly applied generally for that
SoC.

---------

Co-authored-by: Nicolas Munnich <98408764+Nick-Munnich@users.noreply.github.com>

app/src/split/central.c
@@ -1,0 +1,173 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <errno.h>
+
+#include <zmk/stdlib.h>
+#include <zmk/split/transport/central.h>
+#include <zmk/split/central.h>
+#include <zmk/hid_indicators_types.h>
+#include <zmk/pointing/input_split.h>
+
+#include <zephyr/logging/log.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/events/battery_state_changed.h>
+#include <zmk/events/position_state_changed.h>
+#include <zmk/events/sensor_event.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+// TODO: Active transport selection
+
+struct zmk_split_transport_central *active_transport;
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+
+static uint8_t peripheral_battery_levels[ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT] = {0};
+
+#endif // IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+
+int zmk_split_transport_central_peripheral_event_handler(
+    const struct zmk_split_transport_central *transport, uint8_t source,
+    struct zmk_split_transport_peripheral_event ev) {
+    if (transport != active_transport) {
+        // Ignoring events from non-active transport
+        LOG_WRN("Ignoring peripheral event from non-active transport");
+        return -EINVAL;
+    }
+    switch (ev.type) {
+    case ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_KEY_POSITION_EVENT: {
+        struct zmk_position_state_changed state_ev = {.source = source,
+                                                      .position =
+                                                          ev.data.key_position_event.position,
+                                                      .state = ev.data.key_position_event.pressed,
+                                                      .timestamp = k_uptime_get()};
+        return raise_zmk_position_state_changed(state_ev);
+    }
+#if IS_ENABLED(CONFIG_ZMK_INPUT_SPLIT)
+    case ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_INPUT_EVENT: {
+        return zmk_input_split_report_peripheral_event(
+            ev.data.input_event.reg, ev.data.input_event.type, ev.data.input_event.code,
+            ev.data.input_event.value, ev.data.input_event.sync);
+    }
+#endif // IS_ENABLED(CONFIG_ZMK_POINTING)
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+    case ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_BATTERY_EVENT: {
+        struct zmk_peripheral_battery_state_changed battery_ev = {
+            .state_of_charge = ev.data.battery_event.level,
+        };
+        peripheral_battery_levels[source] = ev.data.battery_event.level;
+        return raise_zmk_peripheral_battery_state_changed(battery_ev);
+    }
+#endif
+    case ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_SENSOR_EVENT: {
+        struct zmk_sensor_event sensor_ev = {.sensor_index = ev.data.sensor_event.sensor_index,
+                                             .channel_data_size = 1,
+                                             .timestamp = k_uptime_get()};
+
+        sensor_ev.channel_data[0] = ev.data.sensor_event.channel_data;
+
+        return raise_zmk_sensor_event(sensor_ev);
+    }
+    default:
+        LOG_WRN("GOT AN UNKNOWN EVENT TYPE %d", ev.type);
+        return -ENOTSUP;
+    }
+}
+
+int zmk_split_central_invoke_behavior(uint8_t source, struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event, bool state) {
+    if (!active_transport || !active_transport->api || !active_transport->api->send_command) {
+        return -ENODEV;
+    }
+
+    struct zmk_split_transport_central_command command =
+        (struct zmk_split_transport_central_command){
+            .type = ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_INVOKE_BEHAVIOR,
+            .data =
+                {
+                    .invoke_behavior =
+                        {
+                            .param1 = binding->param1,
+                            .param2 = binding->param2,
+                            .position = event.position,
+                            .event_source = event.source,
+                            .state = state ? 1 : 0,
+                        },
+                },
+        };
+
+    const size_t payload_dev_size = sizeof(command.data.invoke_behavior.behavior_dev);
+    if (strlcpy(command.data.invoke_behavior.behavior_dev, binding->behavior_dev,
+                payload_dev_size) >= payload_dev_size) {
+        LOG_ERR("Truncated behavior label %s to %s before invoking peripheral behavior",
+                binding->behavior_dev, command.data.invoke_behavior.behavior_dev);
+    }
+
+    return active_transport->api->send_command(source, command);
+};
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_PERIPHERAL_HID_INDICATORS)
+
+int zmk_split_central_update_hid_indicator(zmk_hid_indicators_t indicators) {
+    if (!active_transport || !active_transport->api ||
+        !active_transport->api->get_available_source_ids || !active_transport->api->send_command) {
+        return -ENODEV;
+    }
+
+    uint8_t source_ids[ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT];
+
+    int ret = active_transport->api->get_available_source_ids(source_ids);
+
+    if (ret < 0) {
+        return ret;
+    }
+
+    struct zmk_split_transport_central_command command =
+        (struct zmk_split_transport_central_command){
+            .type = ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SET_HID_INDICATORS,
+            .data =
+                {
+                    .set_hid_indicators =
+                        {
+                            .indicators = indicators,
+                        },
+                },
+        };
+
+    for (size_t i = 0; i < ret; i++) {
+        ret = active_transport->api->send_command(source_ids[i], command);
+        if (ret < 0) {
+            return ret;
+        }
+    }
+
+    return 0;
+}
+
+#endif // IS_ENABLED(CONFIG_ZMK_SPLIT_PERIPHERAL_HID_INDICATORS)
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+
+int zmk_split_central_get_peripheral_battery_level(uint8_t source, uint8_t *level) {
+    if (source >= ARRAY_SIZE(peripheral_battery_levels)) {
+        return -EINVAL;
+    }
+
+    *level = peripheral_battery_levels[source];
+    return 0;
+}
+
+#endif // IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
+
+static int central_init(void) {
+    STRUCT_SECTION_GET(zmk_split_transport_central, 0, &active_transport);
+
+    return 0;
+}
+
+SYS_INIT(central_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);

app/src/split/wired/central.c
@@ -1,0 +1,342 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/types.h>
+#include <zephyr/init.h>
+
+#include <zephyr/settings/settings.h>
+#include <zephyr/sys/crc.h>
+#include <zephyr/sys/ring_buffer.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+
+#include <zmk/stdlib.h>
+#include <zmk/behavior.h>
+#include <zmk/sensors.h>
+#include <zmk/split/transport/central.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/position_state_changed.h>
+#include <zmk/events/sensor_event.h>
+#include <zmk/pointing/input_split.h>
+#include <zmk/hid_indicators_types.h>
+#include <zmk/physical_layouts.h>
+
+#include "wired.h"
+
+#define DT_DRV_COMPAT zmk_wired_split
+
+#define IS_HALF_DUPLEX_MODE                                                                        \
+    (DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) && DT_INST_PROP_OR(0, half_duplex, false))
+
+#define RX_BUFFER_SIZE                                                                             \
+    ((sizeof(struct event_envelope) + sizeof(struct msg_postfix)) *                                \
+     CONFIG_ZMK_SPLIT_WIRED_EVENT_BUFFER_ITEMS)
+#define TX_BUFFER_SIZE                                                                             \
+    ((sizeof(struct command_envelope) + sizeof(struct msg_postfix)) *                              \
+     CONFIG_ZMK_SPLIT_WIRED_CMD_BUFFER_ITEMS)
+
+#if IS_HALF_DUPLEX_MODE
+
+static K_SEM_DEFINE(tx_sem, 0, 1);
+
+#endif
+
+RING_BUF_DECLARE(rx_buf, RX_BUFFER_SIZE);
+RING_BUF_DECLARE(tx_buf, TX_BUFFER_SIZE);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static const struct device *uart = DEVICE_DT_GET(DT_INST_PHANDLE(0, device));
+
+#define HAS_DIR_GPIO (IS_HALF_DUPLEX_MODE && DT_INST_NODE_HAS_PROP(0, dir_gpios))
+
+#if HAS_DIR_GPIO
+
+static const struct gpio_dt_spec dir_gpio = GPIO_DT_SPEC_INST_GET(0, dir_gpios);
+
+#endif
+
+#else
+
+#error                                                                                             \
+    "Need to create a node with compatible of 'zmk,wired-split` with a `device` property set to an enabled UART. See http://zmk.dev/docs/development/hardware-integration/new-shield#wired-split"
+
+#endif
+
+static void publish_events_work(struct k_work *work);
+
+K_WORK_DEFINE(publish_events, publish_events_work);
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_ASYNC)
+
+uint8_t async_rx_buf[RX_BUFFER_SIZE / 2][2];
+
+static struct zmk_split_wired_async_state async_state = {
+    .process_tx_work = &publish_events,
+    .rx_bufs = {async_rx_buf[0], async_rx_buf[1]},
+    .rx_bufs_len = RX_BUFFER_SIZE / 2,
+    .rx_size_process_trigger = MSG_EXTRA_SIZE + 1,
+    .rx_buf = &rx_buf,
+    .tx_buf = &tx_buf,
+#if HAS_DIR_GPIO
+    .dir_gpio = &dir_gpio,
+#endif
+};
+
+#endif
+
+#if IS_HALF_DUPLEX_MODE
+
+static int can_tx(void) { return k_sem_take(&tx_sem, K_NO_WAIT); }
+
+#else
+
+static inline int can_tx(void) { return 0; }
+
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_POLLING)
+
+static void send_pending_tx_work_cb(struct k_work *work);
+
+static K_WORK_DEFINE(wired_central_tx_work, send_pending_tx_work_cb);
+
+#endif
+
+static void begin_tx(void) {
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_INTERRUPT)
+    uart_irq_tx_enable(uart);
+#elif IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_ASYNC)
+    zmk_split_wired_async_tx(&async_state);
+#else
+    k_work_submit(&wired_central_tx_work);
+#endif
+}
+
+static ssize_t get_payload_data_size(const struct zmk_split_transport_central_command *cmd) {
+    switch (cmd->type) {
+    case ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_POLL_EVENTS:
+        return 0;
+    case ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_INVOKE_BEHAVIOR:
+        return sizeof(cmd->data.invoke_behavior);
+    case ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SET_PHYSICAL_LAYOUT:
+        return sizeof(cmd->data.set_physical_layout);
+    case ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_SET_HID_INDICATORS:
+        return sizeof(cmd->data.set_hid_indicators);
+    default:
+        return -ENOTSUP;
+    }
+}
+
+static int split_central_wired_send_command(uint8_t source,
+                                            struct zmk_split_transport_central_command cmd) {
+    if (source != 0) {
+        return -EINVAL;
+    }
+
+    ssize_t data_size = get_payload_data_size(&cmd);
+    if (data_size < 0) {
+        LOG_WRN("Failed to determine payload data size %d", data_size);
+        return data_size;
+    }
+
+    // Data + type + source
+    size_t payload_size =
+        data_size + sizeof(source) + sizeof(enum zmk_split_transport_central_command_type);
+
+    if (ring_buf_space_get(&tx_buf) < MSG_EXTRA_SIZE + payload_size) {
+        LOG_WRN("No room to send command to the peripheral %d", source);
+        return -ENOSPC;
+    }
+
+    struct command_envelope env = {.prefix =
+                                       {
+                                           .magic_prefix = ZMK_SPLIT_WIRED_ENVELOPE_MAGIC_PREFIX,
+                                           .payload_size = payload_size,
+                                       },
+                                   .payload = {
+                                       .source = source,
+                                       .cmd = cmd,
+                                   }};
+
+    struct msg_postfix postfix = {.crc =
+                                      crc32_ieee((void *)&env, sizeof(env.prefix) + payload_size)};
+
+    ring_buf_put(&tx_buf, (uint8_t *)&env, sizeof(env.prefix) + payload_size);
+    ring_buf_put(&tx_buf, (uint8_t *)&postfix, sizeof(postfix));
+
+    if (can_tx() >= 0) {
+        begin_tx();
+    }
+
+    return 0;
+}
+
+#if IS_HALF_DUPLEX_MODE
+
+void rx_done_cb(struct k_work *work);
+
+static K_WORK_DELAYABLE_DEFINE(rx_done_work, rx_done_cb);
+
+void rx_done_cb(struct k_work *work) {
+    k_sem_give(&tx_sem);
+
+    // Poll for the next event data!
+    split_central_wired_send_command(0,
+                                     (struct zmk_split_transport_central_command){
+                                         .type = ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_POLL_EVENTS,
+                                     });
+
+    begin_tx();
+
+    k_work_reschedule(&rx_done_work, K_MSEC(CONFIG_ZMK_SPLIT_WIRED_HALF_DUPLEX_RX_TIMEOUT));
+}
+
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_INTERRUPT)
+
+static void serial_cb(const struct device *dev, void *user_data) {
+
+    while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
+        if (uart_irq_rx_ready(dev)) {
+            zmk_split_wired_fifo_read(dev, &rx_buf, &publish_events, NULL);
+#if IS_HALF_DUPLEX_MODE
+            k_work_reschedule(&rx_done_work,
+                              K_TICKS(CONFIG_ZMK_SPLIT_WIRED_HALF_DUPLEX_RX_COMPLETE_TIMEOUT));
+#endif
+        }
+
+        if (uart_irq_tx_complete(dev)) {
+
+            if (ring_buf_size_get(&tx_buf) == 0) {
+                uart_irq_tx_disable(dev);
+#if HAS_DIR_GPIO
+                gpio_pin_set_dt(&dir_gpio, 0);
+#endif
+            }
+        }
+
+        if (uart_irq_tx_ready(dev)) {
+#if HAS_DIR_GPIO
+            gpio_pin_set_dt(&dir_gpio, 1);
+#endif
+            zmk_split_wired_fifo_fill(dev, &tx_buf);
+        }
+    }
+}
+
+#elif IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_POLLING)
+
+static void send_pending_tx_work_cb(struct k_work *work) {
+    zmk_split_wired_poll_out(&tx_buf, uart);
+}
+
+static void read_timer_cb(struct k_timer *_timer) {
+    zmk_split_wired_poll_in(&rx_buf, uart, &publish_events, NULL);
+    // Check if we found any bytes, read some, or read all the bytes in the RX
+}
+
+static K_TIMER_DEFINE(wired_central_read_timer, read_timer_cb, NULL);
+
+#endif
+
+static int zmk_split_wired_central_init(void) {
+    if (!device_is_ready(uart)) {
+        return -ENODEV;
+    }
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_INTERRUPT)
+
+    int ret = uart_irq_callback_user_data_set(uart, serial_cb, NULL);
+
+    if (ret < 0) {
+        if (ret == -ENOTSUP) {
+            LOG_ERR("Interrupt-driven UART API support not enabled");
+        } else if (ret == -ENOSYS) {
+            LOG_ERR("UART device does not support interrupt-driven API");
+        } else {
+            LOG_ERR("Error setting UART callback: %d\n", ret);
+        }
+        return ret;
+    }
+
+    uart_irq_rx_enable(uart);
+
+#elif IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_ASYNC)
+
+    async_state.uart = uart;
+    int ret = zmk_split_wired_async_init(&async_state);
+    if (ret < 0) {
+        LOG_ERR("Failed to set up async wired split UART (%d)", ret);
+        return ret;
+    }
+
+#endif // IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_*)
+
+#if IS_HALF_DUPLEX_MODE
+
+#if HAS_DIR_GPIO
+    LOG_DBG("CONFIGURING AS OUTPUT");
+    gpio_pin_configure_dt(&dir_gpio, GPIO_OUTPUT_INACTIVE);
+#endif
+
+    k_work_schedule(&rx_done_work, K_MSEC(CONFIG_ZMK_SPLIT_WIRED_HALF_DUPLEX_RX_TIMEOUT));
+
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_WIRED_UART_MODE_POLLING)
+    k_timer_start(&wired_central_read_timer, K_TICKS(CONFIG_ZMK_SPLIT_WIRED_POLLING_RX_PERIOD),
+                  K_TICKS(CONFIG_ZMK_SPLIT_WIRED_POLLING_RX_PERIOD));
+#endif
+
+    return 0;
+}
+
+SYS_INIT(zmk_split_wired_central_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+
+static int split_central_wired_get_available_source_ids(uint8_t *sources) {
+    sources[0] = 0;
+
+    return 1;
+}
+
+static const struct zmk_split_transport_central_api central_api = {
+    .send_command = split_central_wired_send_command,
+    .get_available_source_ids = split_central_wired_get_available_source_ids,
+};
+
+ZMK_SPLIT_TRANSPORT_CENTRAL_REGISTER(wired_central, &central_api);
+
+static void publish_events_work(struct k_work *work) {
+
+#if IS_HALF_DUPLEX_MODE
+    k_work_reschedule(&rx_done_work,
+                      K_MSEC(CONFIG_ZMK_SPLIT_WIRED_HALF_DUPLEX_RX_COMPLETE_TIMEOUT));
+#endif // IS_HALF_DUPLEX_MODE
+
+    while (ring_buf_size_get(&rx_buf) > MSG_EXTRA_SIZE) {
+        struct event_envelope env;
+        int item_err =
+            zmk_split_wired_get_item(&rx_buf, (uint8_t *)&env, sizeof(struct event_envelope));
+        switch (item_err) {
+        case 0:
+            zmk_split_transport_central_peripheral_event_handler(&wired_central, env.payload.source,
+                                                                 env.payload.event);
+            break;
+        case -EAGAIN:
+            return;
+        default:
+            LOG_WRN("Issue fetching an item from the RX buffer: %d", item_err);
+            return;
+        }
+    }
+}

app/Kconfig
@@ -189,10 +189,6 @@ config BT_CTLR_PHY_2M
 config BT_TINYCRYPT_ECC
     default y if BT_HCI && !BT_CTLR
 
-config SYSTEM_WORKQUEUE_STACK_SIZE
-    default 4096 if SOC_RP2040
-    default 2048
-
 config ZMK_BLE_THREAD_STACK_SIZE
     int "BLE notify thread stack size"
     default 768

app/Kconfig.defaults
@@ -1,6 +1,10 @@
 # Copyright (c) 2024 The ZMK Contributors
 # SPDX-License-Identifier: MIT
 
+config SYSTEM_WORKQUEUE_STACK_SIZE
+    default 2048 if SOC_RP2040
+    default 2048 if ZMK_BLE
+
 # HID
 if ZMK_HID_REPORT_TYPE_HKRO
 

app/boards/arm/glove80/Kconfig.defconfig
@@ -6,7 +6,7 @@ if BOARD_GLOVE80_LH
 config BOARD
     default "glove80 lh"
 
-config ZMK_SPLIT_BLE_ROLE_CENTRAL
+config ZMK_SPLIT_ROLE_CENTRAL
     default y
 
 endif # BOARD_GLOVE80_LH

app/boards/shields/zmk_uno/zmk_uno_split.dtsi
@@ -11,9 +11,18 @@ left_encoder: &encoder {
     status = "disabled";
 };
 
+&arduino_serial {
+    status = "okay";
+};
+
 / {
     chosen {
         zmk,physical-layout = &split_matrix_physical_layout;
+    };
+
+    wired_split {
+        compatible = "zmk,wired-split";
+        device = <&arduino_serial>;
     };
 
     split_matrix_transform: split_matrix_transform {

app/dts/bindings/zmk,wired-split.yaml
@@ -1,0 +1,21 @@
+# Copyright (c) 2025 The ZMK Contributors
+# SPDX-License-Identifier: MIT
+
+description: |
+  Complete specification of wired split connection
+
+compatible: "zmk,wired-split"
+
+properties:
+  device:
+    type: phandle
+    required: true
+    description: The UART device for wired split communication
+
+  half-duplex:
+    type: boolean
+    description: "Experimental: Enable half-duplex protocol mode"
+
+  dir-gpios:
+    type: phandle-array
+    description: "Experimental: Set the communication direction. Used for RS-422 style comms."

commit 0eabbb931db4aab8429734eb130f2a5810120448

fix(split): add source to battery event
This fixes a bug that was introduced with 147c340.

app/src/split/central.c
@@ -58,6 +58,7 @@ int zmk_split_transport_central_peripheral_event_handler(
 #if IS_ENABLED(CONFIG_ZMK_SPLIT_BLE_CENTRAL_BATTERY_LEVEL_FETCHING)
     case ZMK_SPLIT_TRANSPORT_PERIPHERAL_EVENT_TYPE_BATTERY_EVENT: {
         struct zmk_peripheral_battery_state_changed battery_ev = {
+            .source = source,
             .state_of_charge = ev.data.battery_event.level,
         };
         peripheral_battery_levels[source] = ev.data.battery_event.level;