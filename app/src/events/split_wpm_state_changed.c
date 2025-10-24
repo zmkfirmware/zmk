25532bdf20778f57399af3e2ddf686e374cdd20d
cantruchd <truc294@gmail.com>
2025-10-24 21:44:22 +0800

    Create split_wpm_state_changed.c

---
+++ app/src/events/split_wpm_state_changed.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zmk/events/split_wpm_state_changed.h>
+
+ZMK_EVENT_IMPL(zmk_split_wpm_state_changed);
