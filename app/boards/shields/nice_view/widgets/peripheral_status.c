8151dba1a9cb70179a82d6d1885bf899c48c6cae
cantruchd <truc294@gmail.com>
2025-10-26 07:23:20 +0800

    Update peripheral_status.c

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -207,7 +207,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     // WPM canvas
     lv_obj_t *wpm_canvas = lv_canvas_create(widget->obj);
-    lv_obj_align(wpm_canvas, LV_ALIGN_TOP_LEFT, -48, 0);
+    lv_obj_align(wpm_canvas, LV_ALIGN_TOP_LEFT, 0, 0);
     lv_canvas_set_buffer(wpm_canvas, widget->cbuf2, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     // Top canvas
