9b7415f5690fdf76cdcb0661eda096668d2bf554
cantruchd <truc294@gmail.com>
2025-10-25 19:34:14 +0800

    Update peripheral_status.c

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -5,7 +5,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/random/random.h>
-#include <zephyr/string.h>
+#include <string.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
