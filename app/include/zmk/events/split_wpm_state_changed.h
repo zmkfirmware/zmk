2896cfe821ef3cef86db11fe59c3bf7c9a579f50
cantruchd <truc294@gmail.com>
2025-10-26 20:04:33 +0800

    Update split_wpm_state_changed.h

--- app/include/zmk/events/split_wpm_state_changed.h
+++ app/include/zmk/events/split_wpm_state_changed.h
@@ -1,19 +1,19 @@
-// /*
-//  * Copyright (c) 2025 The ZMK Contributors
-//  *
-//  * SPDX-License-Identifier: MIT
-//  */
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
-// #pragma once
+#pragma once
 
-// #include <zephyr/kernel.h>
-// #include <zmk/event_manager.h>
+#include <zephyr/kernel.h>
+#include <zmk/event_manager.h>
 
-// struct zmk_split_wpm_state_changed {
-//     uint8_t wpm;
-// };
+struct zmk_split_wpm_state_changed {
+    uint8_t wpm;
+};
 
-// ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
+ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
 
 
 // #pragma once
@@ -28,14 +28,14 @@
 
 // ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
 
-#pragma once
+// #pragma once
 
-#include <zephyr/kernel.h>
-#include <zmk/events_manager.h>  // ✅ Quan trọng! Cần include file này để có struct zmk_event_header
+// #include <zephyr/kernel.h>
+// #include <zmk/events_manager.h>  // ✅ Quan trọng! Cần include file này để có struct zmk_event_header
 
-struct zmk_split_wpm_state_changed {
-    struct zmk_event_header header;
-    uint8_t wpm;
-};
+// struct zmk_split_wpm_state_changed {
+//     struct zmk_event_header header;
+//     uint8_t wpm;
+// };
 
-ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
+// ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
