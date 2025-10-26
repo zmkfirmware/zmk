f7f547c3dc6163480fac64a3e2e6b9ad3ed49b81
cantruchd <truc294@gmail.com>
2025-10-26 19:39:27 +0800

    Update split_wpm_state_changed.h

--- app/include/zmk/events/split_wpm_state_changed.h
+++ app/include/zmk/events/split_wpm_state_changed.h
@@ -16,10 +16,22 @@
 // ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
 
 
+// #pragma once
+
+// #include <zephyr/kernel.h>
+// #include <zmk/event_manager.h>
+
+// struct zmk_split_wpm_state_changed {
+//     struct zmk_event_header header;
+//     uint8_t wpm;
+// };
+
+// ZMK_EVENT_DECLARE(zmk_split_wpm_state_changed);
+
 #pragma once
 
 #include <zephyr/kernel.h>
-#include <zmk/event_manager.h>
+#include <zmk/events.h>  // ✅ Quan trọng! Cần include file này để có struct zmk_event_header
 
 struct zmk_split_wpm_state_changed {
     struct zmk_event_header header;
