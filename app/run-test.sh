3e42830f781d64f7552935848086d945ebc9ec54
Peter Johanson <peter@peterjohanson.com>
2025-12-15 15:01:01 -0700

    refactor(core): Adjust our approach for upstream Zephyr boards
    Move to using proper HWMv2 board extensions
    https://docs.zephyrproject.org/4.1.0/hardware/porting/board_porting.html#board-extensions
    
    for extending upstream Zephyr boards for use with ZMK. With this change,
    using upstream Zephyr board IDs directly, e.g. `seeeduino_xiao` will be
    stock versions as found upstream. To use a board variant that is tuned
    for ZMK use, use the `zmk` variant, e.g. `seeeduino_xiao//zmk` which is
    shorthand for `seeeduino_xiao/samd21g18a/zmk`.

--- app/run-test.sh
+++ app/run-test.sh
@@ -38,7 +38,7 @@ testcase=$(realpath $path | sed -n -e "s|.*/tests/||p")
 echo "Running $testcase:"
 
 build_cmd="west build ${ZMK_SRC_DIR:+-s $ZMK_SRC_DIR} -d ${ZMK_BUILD_DIR}/tests/$testcase \
-    -b native_sim/native/64 -p -- -DCONFIG_ASSERT=y -DZMK_CONFIG="$(realpath $path)" \
+    -b native_sim//zmk_test_mock -p -- -DCONFIG_ASSERT=y -DZMK_CONFIG="$(realpath $path)" \
     ${ZMK_EXTRA_MODULES:+-DZMK_EXTRA_MODULES="$(realpath ${ZMK_EXTRA_MODULES})"}"
 
 build_log_tmp="${ZMK_BUILD_DIR}/tmp/$testcase/build.log"
