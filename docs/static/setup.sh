4dfbb1c702deeee1b9b2a633d2c294855e92356c
Nicolas Munnich <98408764+nmunnich@users.noreply.github.com>
2025-12-29 17:26:41 +0000

    Update docs/static/setup.sh
    Co-authored-by: Cem Aksoylar <caksoylar@users.noreply.github.com>

--- docs/static/setup.sh
+++ docs/static/setup.sh
@@ -1,1 +1,2 @@
-echo "This setup script has been removed. Please use the ZMK CLI instead. Visit https://zmk.dev/docs/user-setup for more information."
+echo "This setup script has been removed. Please use the ZMK CLI instead."
+echo "Visit https://zmk.dev/docs/user-setup for more information."
