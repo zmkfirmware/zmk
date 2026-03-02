b3f243bb40a80ff40d4435836fe17dabeabd03e1
André A. Gomes <andremegafone@gmail.com>
2026-03-02 21:39:08 +0200

    docs: Update Linux compatibility of C_AC_NEXT_KEYBOARD_LAYOUT_SELECT (#3268)

--- docs/src/data/hid.js
+++ docs/src/data/hid.js
@@ -7897,7 +7897,7 @@ export default [
     documentation: "https://usb.org/sites/default/files/hut1_2.pdf#page=153",
     os: {
       windows: null,
-      linux: null,
+      linux: false,
       android: null,
       macos: true,
       ios: true,
