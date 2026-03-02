371fb7912f7d5f7816ce7d4f262b1e81c1b48f02
Hanks <hszheng47@gmail.com>
2026-03-02 00:43:02 -0800

    add boards/hanks/hnkb40

---
+++ app/boards/hanks/hnkb40/README.md
@@ -0,0 +1,9 @@
+# HNKB40
+
+
+
+## Building hnkb40-lp ZMK firmware
+
+```
+west build -p -b hnkb40
+```
---
+++ app/boards/hanks/hnkb40/board.cmake
@@ -0,0 +1,8 @@
+# Copyright (c) 2021 Nick Winans
+# SPDX-License-Identifier: MIT
+
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
