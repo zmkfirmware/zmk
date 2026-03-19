fd67eb226b2bde4e956a3dbf36a8a8a990639e9c
newx - Isaac Turner <156273295+newx-it@users.noreply.github.com>
2026-03-19 08:49:08 -0600

    fix: syntax error
    fixed an error caused by an extra line of code that wasn't deleted

--- app/src/activity.c
+++ app/src/activity.c
@@ -82,12 +82,11 @@ void activity_work_handler(struct k_work *work) {
     bool prevent_sleep = 
 	!IS_ENABLED(CONFIG_ZMK_USB_ALLOW_SLEEP_WHILE_POWERED) && is_usb_power_present();
     #if IS_ENABLED(CONFIG_ZMK_BLE_PREVENT_SLEEP_WHILE_ACTIVE_CONNECTED)
-        prevent_sleep = prevent_sleep ||
-            #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-                prevent_sleep |= zmk_ble_active_profile_is_connected();
-            #else
-                prevent_sleep |= zmk_split_bt_peripheral_is_connected();
-            #endif
+        #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+            prevent_sleep |= zmk_ble_active_profile_is_connected();
+        #else
+            prevent_sleep |= zmk_split_bt_peripheral_is_connected();
+        #endif
     #endif
     if (inactive_time > MAX_SLEEP_MS && !prevent_sleep) {
         // Put devices in suspend power mode before sleeping
