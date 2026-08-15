29799fad5b88e9cba4659e508d7fdbcc648a14a9
ClicketySplit <dev@clicketysplit.ca>
2026-08-15 03:39:40 +0000

    feat(boards): add Clickety Split Chiisai

---
+++ app/boards/clickety_split/clickety_split_chiisai/pre_dt_board.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025 The ZMK Contributors
+# SPDX-License-Identifier: MIT
+#
+
+# Suppress duplicate unit-address warnings for overlapping Nordic peripherals.
+# https://docs.zephyrproject.org/latest/build/dts/intro-input-output.html
+
+list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
